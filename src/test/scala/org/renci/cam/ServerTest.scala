174aab33df79d34900b457267bdfb451b2e5d56b
Jim Balhoff <jim@balhoff.org>
2020-06-18 14:19:16 -0400

    Use our QueryService to make the ZIO-based http client. The test runtime should provide all the needed execution context. Don’t need a ZIO effect to construct a Request.

--- src/test/scala/org/renci/cam/ServerTest.scala
+++ src/test/scala/org/renci/cam/ServerTest.scala
@@ -10,7 +10,6 @@ import io.circe._
 import zio.interop.catz._
 import zio.interop.catz.implicits._
 import cats.effect.Blocker
-import org.renci.cam.TestHelpers._
 import org.http4s._
 import org.http4s.headers._
 import zio.test.Assertion.equalTo
@@ -57,25 +56,14 @@ object ServerTest extends DefaultRunnableSpec {
 }"""
 
         for {
-          request <- ZIO.effect(
-            Request[Task](Method.POST, Uri.uri("http://127.0.0.1:8080/query?limit=1&"))
-              .withHeaders(Accept.parse("application/json").toOption.get,
-                           `Content-Type`.parse("application/x-www-form-urlencoded").toOption.get)
-              .withEntity(body)
-          )
-          response <- {
-            val blockingPool = Executors.newFixedThreadPool(5)
-            val blocker = Blocker.liftExecutorService(blockingPool)
-            val httpClient: Client[Task] = JavaNetClientBuilder[Task](blocker).create
-            httpClient.expect[String](request)
-          }
+          clientManaged <- QueryService.makeHttpClient
+          request = Request[Task](Method.POST, Uri.uri("http://127.0.0.1:8080/query?limit=1&"))
+            .withHeaders(Accept.parse("application/json").toOption.get,
+                         `Content-Type`.parse("application/x-www-form-urlencoded").toOption.get)
+            .withEntity(body)
+          response <- clientManaged.use(_.expect[String](request))
         } yield assert(response)(equalTo(""))
       }
     )
 
-}
-
-object TestHelpers {
-  val blockingEC = ExecutionContext.fromExecutorService(Executors.newFixedThreadPool(5))
-  val blocker = Blocker.liftExecutionContext(blockingEC)
 }
