3660913b0e92a653d6276570393f46d571003e22
Jason Reilly <jdr0887@gmail.com>
2020-07-17 15:39:01 -0400

    moving stuff around

--- src/main/scala/org/renci/cam/domain/package.scala
+++ src/main/scala/org/renci/cam/domain/package.scala
@@ -4,15 +4,15 @@ package object domain {
 
   case class TranslatorQueryNode(id: String, `type`: String, curie: Option[String])
 
-  case class TranslatorQueryEdge(id: String, source_id: String, target_id: String, `type`: String)
+  case class TranslatorQueryEdge(id: String, `type`: String, source_id: String, target_id: String)
 
   case class TranslatorQueryGraph(nodes: List[TranslatorQueryNode], edges: List[TranslatorQueryEdge])
 
   case class TranslatorNodeAttribute(name: String, value: String, `type`: String, url: Option[String], source: Option[String])
 
-  case class TranslatorNode(`type`: String, name: Option[String], attributes: Option[List[TranslatorNodeAttribute]])
+  case class TranslatorNode(id: String, name: Option[String], `type`: Option[List[String]], attributes: Option[List[TranslatorNodeAttribute]])
 
-  case class TranslatorEdge(id: String, source_id: String, target_id: String, `type`: Option[String])
+  case class TranslatorEdge(id: String, `type`: Option[String], source_id: String, target_id: String)
 
   case class TranslatorKnowledgeGraph(nodes: List[TranslatorNode], edges: List[TranslatorEdge])
 
@@ -22,7 +22,7 @@ package object domain {
 
   case class TranslatorResult(node_bindings: List[TranslatorNodeBinding], edge_bindings: List[TranslatorEdgeBinding])
 
-  case class TranslatorMessage(results: Option[List[TranslatorResult]], query_graph: Option[TranslatorQueryGraph], knowledge_graph: Option[TranslatorKnowledgeGraph])
+  case class TranslatorMessage(query_graph: Option[TranslatorQueryGraph], knowledge_graph: Option[TranslatorKnowledgeGraph], results: Option[List[TranslatorResult]])
 
   case class TranslatorQueryRequestBody(message: TranslatorMessage)
 
