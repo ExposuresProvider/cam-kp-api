30b060f620b7fddefb9244950adf11c5a195016e
Jason Reilly <jdr0887@gmail.com>
2020-08-06 10:53:11 -0400

    formatting, adding optional extra nodes/edges

--- src/main/scala/org/renci/cam/domain/package.scala
+++ src/main/scala/org/renci/cam/domain/package.scala
@@ -8,21 +8,26 @@ package object domain {
 
   case class TranslatorQueryGraph(nodes: List[TranslatorQueryNode], edges: List[TranslatorQueryEdge])
 
-  case class TranslatorNodeAttribute(name: String, value: String, `type`: String, url: Option[String], source: Option[String])
+  case class TranslatorNodeAttribute(name: Option[String], value: String, `type`: String, url: Option[String], source: Option[String])
 
-  case class TranslatorNode(id: String, name: Option[String], `type`: List[String], attributes: List[TranslatorNodeAttribute])
+  case class TranslatorNode(name: Option[String], `type`: List[String], attributes: List[TranslatorNodeAttribute])
 
   case class TranslatorEdge(id: String, `type`: Option[String], source_id: String, target_id: String)
 
   case class TranslatorKnowledgeGraph(nodes: List[TranslatorNode], edges: List[TranslatorEdge])
 
-  case class TranslatorNodeBinding(qg_id: String, kg_id: String)
+  case class TranslatorNodeBinding(qg_id: Option[String], kg_id: String)
 
-  case class TranslatorEdgeBinding(qg_id: String, kg_id: String, provenance: Option[String])
+  case class TranslatorEdgeBinding(qg_id: Option[String], kg_id: String, provenance: Option[String])
 
-  case class TranslatorResult(node_bindings: List[TranslatorNodeBinding], edge_bindings: List[TranslatorEdgeBinding])
+  case class TranslatorResult(node_bindings: List[TranslatorNodeBinding],
+                              edge_bindings: List[TranslatorEdgeBinding],
+                              extra_nodes: Option[List[TranslatorNodeBinding]],
+                              extra_edges: Option[List[TranslatorEdgeBinding]])
 
-  case class TranslatorMessage(query_graph: Option[TranslatorQueryGraph], knowledge_graph: Option[TranslatorKnowledgeGraph], results: List[TranslatorResult])
+  case class TranslatorMessage(query_graph: Option[TranslatorQueryGraph],
+                               knowledge_graph: Option[TranslatorKnowledgeGraph],
+                               results: List[TranslatorResult])
 
   case class TranslatorQueryRequestBody(message: TranslatorMessage)
 
