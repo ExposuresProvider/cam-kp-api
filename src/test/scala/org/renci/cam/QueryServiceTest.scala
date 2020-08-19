68b9888e1bd02267d3b841244114b2aaf9f52eb9
Jason Reilly <jdr0887@gmail.com>
2020-08-19 13:19:14 -0400

    adding test to address issue 73

--- src/test/scala/org/renci/cam/QueryServiceTest.scala
+++ src/test/scala/org/renci/cam/QueryServiceTest.scala
@@ -21,23 +21,50 @@ object QueryServiceTest extends DefaultRunnableSpec {
   def spec =
     suite("QueryServiceSpec")(
       test("testGetNodeTypes") {
-        val n0Node = TRAPIQueryNode("n0", "gene", Some("NCBIGENE:558"))
-        val n1Node = TRAPIQueryNode("n1", "biological_process", None)
-        val e0Edge = TRAPIQueryEdge("e0", "has_participant", "n1", "n0")
+        val n0Node = TRAPIQueryNode("n0", Some("gene"), Some("NCBIGENE:558"))
+        val n1Node = TRAPIQueryNode("n1", Some("biological_process"), None)
+        val e0Edge = TRAPIQueryEdge("e0", "n1", "n0", Some("has_participant"))
 
         val queryGraph = TRAPIQueryGraph(List(n0Node, n1Node), List(e0Edge))
         val map = QueryService.getNodeTypes(queryGraph.nodes)
         map.foreach(a => printf("k: %s, v: %s%n", a._1, a._2))
         assert(map)(isNonEmpty)
       } @@ ignore,
-      testM("query service") {
+      testM("test simple query") {
+        for {
+          httpClient <- SPARQLQueryExecutor.makeHttpClient
+          encoded = {
+            val n0Node = TRAPIQueryNode("n0", Some("gene"), None)
+            val n1Node = TRAPIQueryNode("n1", Some("biological_process"), None)
+            val e0Edge = TRAPIQueryEdge("e0", "n1", "n0", Some("has_participant"))
 
-        val n0Node = TRAPIQueryNode("n0", "gene", None /*Some("NCBIGENE:558")*/ )
-        val n1Node = TRAPIQueryNode("n1", "biological_process", None)
-        val e0Edge = TRAPIQueryEdge("e0", "has_participant", "n1", "n0")
-
-        val queryGraph = TRAPIQueryGraph(List(n0Node, n1Node), List(e0Edge))
-        val message = TRAPIMessage(Some(queryGraph), None, List[TRAPIResult]())
+            val queryGraph = TRAPIQueryGraph(List(n0Node, n1Node), List(e0Edge))
+            val message = TRAPIMessage(Some(queryGraph), None, None)
+            val requestBody = TRAPIQueryRequestBody(message)
+            requestBody.asJson.deepDropNullValues.noSpaces
+          }
+          _ = println("encoded: " + encoded)
+          uri = uri"http://127.0.0.1:8080/query".withQueryParam("limit", 1) // scala
+          //uri = uri"http://127.0.0.1:6434/query".withQueryParam("limit", 1) // python
+          request = Request[Task](Method.POST, uri)
+            .withHeaders(Accept(MediaType.application.json), `Content-Type`(MediaType.application.json))
+            .withEntity(encoded)
+          response <- httpClient.use(_.expect[String](request))
+          _ = println("response: " + response)
+          parsed <- Task.effect(parse(response).getOrElse(Json.Null))
+          _ = Files.write(Paths.get("src/test/resources/local-scala.json"), parsed.as[String].toOption.get.getBytes)
+        } yield assert(response)(isNonEmptyString)
+      } /*@@ ignore*/,
+      testM("test gene to gene") {
+        val n0Node = TRAPIQueryNode("n0", Some("gene"), Some("UniProtKB:P30530") )
+        val n1Node = TRAPIQueryNode("n1", Some("biological_process"), None)
+        val n2Node = TRAPIQueryNode("n2", Some("biological_process"), None)
+        val n3Node = TRAPIQueryNode("n3", Some("gene"), None)
+        val e0Edge = TRAPIQueryEdge("e0", "n0", "n1", None)
+        val e1Edge = TRAPIQueryEdge("e1", "n1", "n2", Some("enabled_by"))
+        val e2Edge = TRAPIQueryEdge("e2", "n2", "n3", None)
+        val queryGraph = TRAPIQueryGraph(List(n0Node, n1Node, n2Node, n3Node), List(e0Edge, e1Edge, e2Edge))
+        val message = TRAPIMessage(Some(queryGraph), None, None)
         val requestBody = TRAPIQueryRequestBody(message)
         val encoded = requestBody.asJson.deepDropNullValues.noSpaces
         for {
@@ -52,7 +79,7 @@ object QueryServiceTest extends DefaultRunnableSpec {
           parsed <- Task.effect(parse(response).getOrElse(Json.Null))
           _ = Files.write(Paths.get("src/test/resources/local-scala.json"), parsed.as[String].toOption.get.getBytes)
         } yield assert(response)(isNonEmptyString)
-      } @@ ignore
+      } @@ ignore,
     )
 
 }
