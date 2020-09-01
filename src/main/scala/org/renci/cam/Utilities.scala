61b1a6ea8d69ca319a5ec06f0e8f4037fb7a963e
Shalsh <shrivastava.shal@gmail.com>
2020-09-01 16:16:43 -0400

    Implement fallback from file

--- src/main/scala/org/renci/cam/Utilities.scala
+++ src/main/scala/org/renci/cam/Utilities.scala
@@ -1,6 +1,8 @@
 package org.renci.cam
 
+import scala.io.Source._
 import io.circe.Json
+import io.circe.parser.parse
 import org.http4s.circe._
 import org.http4s.headers.Accept
 import org.http4s.implicits._
@@ -11,7 +13,7 @@ import zio.interop.catz._
 
 object Utilities {
 
-  def getBiolinkPrefixes: ZIO[HttpClient, Throwable, Map[String, String]] =
+  def getBiolinkPrefixesFromURL: ZIO[HttpClient, Throwable, PrefixesMap] =
     for {
       httpClient <- HttpClient.client
       uri = uri"https://biolink.github.io/biolink-model/context.jsonld"
@@ -27,6 +29,29 @@ object Utilities {
           .collect {
             case (key, Right(value)) => (key, value)
           }
-    } yield curies
+    } yield PrefixesMap(curies)
+
+  def getBiolinkPrefixesFromFile: ZIO[Any, Throwable, PrefixesMap] = for {
+    val prefixesStr = fromFile("prefixes.json").mkString
+    val prefixesJson = parse(prefixesStr)
+    val cursor = prefixesJson.hcursor
+    val contextValue = ZIO.fromEither(cursor.downField("@context").as[Map[String, Json]])
+    val curies =
+      contextValue
+        .map {
+          case (key, value) => (key, value.as[String])
+        }
+        .collect {
+          case (key, Right(value)) => (key, value)
+        }
+  } yield curies //getResourcesAsStream
+
+  def getPrefixes: ZIO[HttpClient, Throwable, PrefixesMap] = getBiolinkPrefixesFromURL.orElse(getBiolinkPrefixesFromFile)
+
+  def makePrefixesLayer: UIO[TaskLayer[Has[PrefixesMap]]] = getBiolinkPrefixesFromURL.map(ZLayer.fromManaged())
+
+  val biolinkPrefixes: ZIO[HttpClient, Nothing, TaskLayer[Has[PrefixesMap]]] = ZIO.service
+
+  case class PrefixesMap(prefixesMap: Map[String, String])
 
 }
