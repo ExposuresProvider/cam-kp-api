950cc71fc8c62897dc2423f41862c6bb9acda236
Jason Reilly <jdr0887@gmail.com>
2020-10-23 18:56:23 -0400

    moving encoders/decoders to Implicits

--- src/main/scala/org/renci/cam/domain/package.scala
+++ src/main/scala/org/renci/cam/domain/package.scala
@@ -1,41 +1,10 @@
 package org.renci.cam
 
-import io.circe.Decoder.Result
-import io.circe.{Decoder, DecodingFailure, Encoder, HCursor}
-import org.apache.commons.text.CaseUtils
-import org.apache.jena.ext.com.google.common.base.CaseFormat
+import com.google.common.base.CaseFormat
 
 package object domain {
 
-  final case class IRI(value: String)
-
-  object IRI {
-
-    private val Curie = "^([^:]*):(.*)$".r
-
-    def makeDecoder(prefixesMap: Map[String, String]): Decoder[IRI] = new Decoder[IRI] {
-
-      private val protocols = Set("http", "https", "ftp", "file", "mailto")
-
-      override def apply(c: HCursor): Result[IRI] = for {
-        value <- c.value.as[String]
-        Curie(prefix, local) = value
-        namespace <-
-          if (protocols(prefix)) Right(prefix)
-          else prefixesMap.get(prefix).toRight(DecodingFailure(s"No prefix expansion found for $prefix:$local", Nil))
-      } yield IRI(s"$namespace$local")
-
-    }
-
-    def makeEncoder(prefixesMap: Map[String, String]): Encoder[IRI] = Encoder.encodeString.contramap { iri =>
-      val startsWith = prefixesMap.filter { case (_, namespace) => iri.value.startsWith(namespace) }
-      if (startsWith.nonEmpty) {
-        val (prefix, namespace) = startsWith.maxBy(_._2.length)
-        s"$prefix:${iri.value.drop(namespace.length)}"
-      } else iri.value
-    }
-
-  }
+  case class IRI(value: String)
 
   sealed trait BiolinkTerm {
 
@@ -50,18 +19,9 @@ package object domain {
   object BiolinkClass {
 
     def apply(label: String): BiolinkClass = {
-      val underscoreLabel = label.replaceAllLiterally(" ", "_")
-      val camelLabel = CaseFormat.LOWER_UNDERSCORE.to(CaseFormat.UPPER_CAMEL, underscoreLabel)
-      BiolinkClass(camelLabel, IRI(s"${BiolinkTerm.namespace}$camelLabel"))
+      val camelLabel = CaseFormat.LOWER_UNDERSCORE.to(CaseFormat.UPPER_CAMEL, label)
+      BiolinkClass(label, IRI(s"${BiolinkTerm.namespace}$camelLabel"))
     }
-
-    //FIXME would be good to check that this is a known Biolink term rather than just accepting
-    implicit val decoder: Decoder[BiolinkClass] = Decoder.decodeString.map { s =>
-      val local = CaseUtils.toCamelCase(s, true, '_')
-      BiolinkClass(s, IRI(s"${BiolinkTerm.namespace}$local"))
-    }
-
-    implicit val encoder: Encoder[BiolinkClass] = Encoder.encodeString.contramap(blTerm => blTerm.shorthand)
 
   }
 
@@ -70,16 +30,8 @@ package object domain {
   object BiolinkPredicate {
 
     def apply(label: String): BiolinkPredicate = {
-      val underscoreLabel = label.replaceAllLiterally(" ", "_")
-      BiolinkPredicate(underscoreLabel, IRI(s"${BiolinkTerm.namespace}$underscoreLabel"))
+      BiolinkPredicate(label, IRI(s"${BiolinkTerm.namespace}$label"))
     }
-
-    //FIXME would be good to check that this is a known Biolink term rather than just accepting
-    implicit val decoder: Decoder[BiolinkPredicate] = Decoder.decodeString.map { s =>
-      BiolinkPredicate(s, IRI(s"${BiolinkTerm.namespace}$s"))
-    }
-
-    implicit val encoder: Encoder[BiolinkPredicate] = Encoder.encodeString.contramap(blTerm => blTerm.shorthand)
 
   }
 
