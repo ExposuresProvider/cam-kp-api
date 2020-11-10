b5b65c785844c50a6c601c7f5f1e6d946080aca2
Jim Balhoff <jim@balhoff.org>
2020-11-10 15:50:33 -0500

    Remove thrown exceptions.

--- src/main/scala/org/renci/cam/QueryService.scala
+++ src/main/scala/org/renci/cam/QueryService.scala
@@ -54,14 +54,11 @@ object QueryService extends LazyLogging {
   private def convertCase(v: String): String = CaseFormat.UPPER_CAMEL.to(CaseFormat.LOWER_UNDERSCORE, v)
 
   def getNodeTypes(nodes: List[TRAPIQueryNode]): Map[String, IRI] =
-    nodes
-      .map(node =>
-        (node.`type`, node.curie) match {
-          case (_, Some(c))    => (node.id, c)
-          case (Some(t), None) => (node.id, t.iri)
-          case (None, None) => throw new Exception("both type and curie can't be None")
-        })
-      .toMap
+    nodes.map {node => (node.`type`, node.curie) match {
+          case (_, Some(c))    => Map(node.id -> c)
+          case (Some(t), None) => Map(node.id-> t.iri)
+          case (None, None) => Map.empty[String, IRI]
+        }}.fold(Map.empty[String, IRI])(_ ++ _)
 
   def applyPrefix(value: String, prefixes: Map[String, String]): String =
     prefixes
@@ -311,9 +308,7 @@ object QueryService extends LazyLogging {
                                   biolinkClasses: List[BiolinkClass]): RIO[ZConfig[AppConfig] with HttpClient, List[TripleString]] =
     for {
       nodeIds <- Task.effect(QueryText(nodeIdList.mkString(" ")))
-      namedThingBiolinkClass = biolinkClasses
-        .find(a => a.shorthand == "named_thing")
-        .getOrElse(throw new Exception("Could not find BiolinkClass:NamedThing"))
+      namedThingBiolinkClass <- ZIO.fromOption(biolinkClasses.find(a => a.shorthand == "named_thing")).orElseFail(new Exception("Could not find BiolinkClass:NamedThing"))
       queryText = sparql"""SELECT DISTINCT ?kid ?blclass ?label WHERE { VALUES ?kid { $nodeIds }
                      ?kid $rdfSchemaSubClassOf ?blclass .
                      ?blclass $biolinkmlIsA* ${namedThingBiolinkClass.iri} .
