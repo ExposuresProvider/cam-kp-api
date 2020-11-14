545af3aac33365c6a21a71fb69f6c8e7882ab1c6
Jim Balhoff <jim@balhoff.org>
2020-11-14 08:58:36 -0500

    Formatting.

--- src/main/scala/org/renci/cam/Server.scala
+++ src/main/scala/org/renci/cam/Server.scala
@@ -9,8 +9,8 @@ import org.http4s.implicits._
 import org.http4s.server.Router
 import org.http4s.server.blaze.BlazeServerBuilder
 import org.http4s.server.middleware.{Logger, _}
-import org.renci.cam.HttpClient.HttpClient
 import org.renci.cam.Biolink._
+import org.renci.cam.HttpClient.HttpClient
 import org.renci.cam.domain._
 import sttp.tapir.docs.openapi._
 import sttp.tapir.json.circe._
@@ -19,8 +19,8 @@ import sttp.tapir.server.http4s.ztapir._
 import sttp.tapir.swagger.http4s.SwaggerHttp4s
 import sttp.tapir.ztapir._
 import zio._
-import zio.config.{ZConfig, getConfig}
 import zio.config.typesafe.TypesafeConfig
+import zio.config.{getConfig, ZConfig}
 import zio.interop.catz._
 import zio.interop.catz.implicits._
 
@@ -43,42 +43,44 @@ object Server extends App with LazyLogging {
     program.mapError(error => error.getMessage)
   }
 
-  val queryEndpointZ: URIO[Has[BiolinkData], ZEndpoint[(Int, TRAPIQueryRequestBody), String, TRAPIMessage]] = {
+  val queryEndpointZ: URIO[Has[BiolinkData], ZEndpoint[(Int, TRAPIQueryRequestBody), String, TRAPIMessage]] =
     for {
       biolinkData <- biolinkData
-    } yield {
-      endpoint.post
-        .in("query")
-        .in(query[Int]("limit"))
-        .in({
+    } yield endpoint.post
+      .in("query")
+      .in(query[Int]("limit"))
+      .in(
+        {
           implicit val iriDecoder: Decoder[IRI] = Implicits.iriDecoder(biolinkData.prefixes)
           implicit val biolinkClassDecoder: Decoder[BiolinkClass] = Implicits.biolinkClassDecoder(biolinkData.classes)
           implicit val biolinkPredicateDecoder: Decoder[BiolinkPredicate] = Implicits.biolinkPredicateDecoder(biolinkData.predicates)
           jsonBody[TRAPIQueryRequestBody]
-        })
-        .errorOut(stringBody)
-        .out({
+        }
+      )
+      .errorOut(stringBody)
+      .out(
+        {
           implicit val iriEncoder: Encoder[IRI] = Implicits.iriEncoderOut(biolinkData.prefixes)
           implicit val biolinkClassEncoder: Encoder[BiolinkClass] = Encoder.encodeString.contramap(blTerm => blTerm.shorthand)
           implicit val biolinkPredicateEncoder: Encoder[BiolinkPredicate] = Encoder.encodeString.contramap(blTerm => blTerm.shorthand)
           jsonBody[TRAPIMessage]
-        })
-        .summary("Submit a TRAPI question graph and retrieve matching solutions")
-    }
-  }
+        }
+      )
+      .summary("Submit a TRAPI question graph and retrieve matching solutions")
 
   def queryRouteR(queryEndpoint: ZEndpoint[(Int, TRAPIQueryRequestBody), String, TRAPIMessage])
-    : URIO[ZConfig[AppConfig] with HttpClient with Has[BiolinkData], HttpRoutes[Task]] = queryEndpoint.toRoutesR { case (limit, body) =>
-    val program = for {
-      queryGraph <-
-        ZIO
-          .fromOption(body.message.query_graph)
-          .orElseFail(new InvalidBodyException("A query graph is required, but hasn't been provided."))
-      resultSet <- QueryService.run(limit, queryGraph)
-      message <- QueryService.parseResultSet(queryGraph, resultSet)
-    } yield message
-    program.mapError(error => error.getMessage)
-  }
+    : URIO[ZConfig[AppConfig] with HttpClient with Has[BiolinkData], HttpRoutes[Task]] =
+    queryEndpoint.toRoutesR { case (limit, body) =>
+      val program = for {
+        queryGraph <-
+          ZIO
+            .fromOption(body.message.query_graph)
+            .orElseFail(new InvalidBodyException("A query graph is required, but hasn't been provided."))
+        resultSet <- QueryService.run(limit, queryGraph)
+        message <- QueryService.parseResultSet(queryGraph, resultSet)
+      } yield message
+      program.mapError(error => error.getMessage)
+    }
 
   val server: RIO[ZConfig[AppConfig] with HttpClient with Has[BiolinkData], Unit] =
     ZIO.runtime[Any].flatMap { implicit runtime =>
