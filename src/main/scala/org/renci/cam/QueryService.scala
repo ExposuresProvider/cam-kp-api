4d55f0bb4c31a363a83322af2337f8d53a5f43d0
Jason Reilly <jdr0887@gmail.com>
2020-11-19 10:39:55 -0500

    using Predicate only in getTRAPIQEdgePredicates

--- src/main/scala/org/renci/cam/QueryService.scala
+++ src/main/scala/org/renci/cam/QueryService.scala
@@ -76,7 +76,7 @@ object QueryService extends LazyLogging {
       predicates <- ZIO.foreachPar(queryGraph.edges.filter(_.`type`.nonEmpty))(edge => {
         for {
           foundPredicates <- getTRAPIQEdgePredicates(edge)
-          predicates = foundPredicates.map(a => sparql" ${a.predicate} ").fold(sparql"")(_ + _)
+          predicates = foundPredicates.map(a => sparql" $a ").fold(sparql"")(_ + _)
           edgeIDVar = Var(edge.id)
           edgeSourceVar = Var(edge.source_id)
           edgeTargetVar = Var(edge.target_id)
@@ -383,7 +383,7 @@ object QueryService extends LazyLogging {
 
   final private case class Predicate(predicate: IRI)
 
-  private def getTRAPIQEdgePredicates(edge: TRAPIQueryEdge): RIO[ZConfig[AppConfig] with HttpClient, List[Predicate]] =
+  private def getTRAPIQEdgePredicates(edge: TRAPIQueryEdge): RIO[ZConfig[AppConfig] with HttpClient, List[IRI]] =
     for {
       edgeType <- ZIO.fromOption(edge.`type`).orElseFail(new Exception("failed to get edge type"))
       queryText = sparql"""
@@ -394,6 +394,6 @@ object QueryService extends LazyLogging {
                    }
                    """
       predicates <- SPARQLQueryExecutor.runSelectQueryAs[Predicate](queryText.toQuery)
-    } yield predicates
+    } yield predicates.map(_.predicate)
 
 }
