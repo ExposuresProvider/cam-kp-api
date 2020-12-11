863611ff40cef086279fcb2a2e81e771a96c308e
Jason Reilly <jdr0887@gmail.com>
2020-12-11 11:51:39 -0500

    returning map type

--- src/main/scala/org/renci/cam/PredicatesService.scala
+++ src/main/scala/org/renci/cam/PredicatesService.scala
@@ -4,8 +4,6 @@ import java.io.StringReader
 import java.nio.file.{Files, Paths}
 
 import com.typesafe.scalalogging.LazyLogging
-import io.circe.{Encoder, KeyEncoder}
-import io.circe.syntax._
 import org.apache.commons.csv.CSVFormat
 import org.renci.cam.Biolink.BiolinkData
 import org.renci.cam.HttpClient.HttpClient
@@ -19,7 +17,7 @@ object PredicatesService extends LazyLogging {
 
   case class Triple(subj: BiolinkClass, pred: BiolinkPredicate, obj: BiolinkClass)
 
-  def run: RIO[ZConfig[AppConfig] with HttpClient with Has[BiolinkData], String] =
+  def run: RIO[ZConfig[AppConfig] with HttpClient with Has[BiolinkData], Map[BiolinkClass, Map[BiolinkClass, List[BiolinkPredicate]]]] =
     for {
       predicates <- Task.effect(Files.readString(Paths.get(getClass.getClassLoader.getResource("predicates.csv").toURI)))
       records = CSVFormat.DEFAULT.parse(new StringReader(predicates)).getRecords
@@ -27,11 +25,6 @@ object PredicatesService extends LazyLogging {
         .map(a => PredicatesService.Triple(BiolinkClass(a.get(0)), BiolinkPredicate(a.get(1)), BiolinkClass(a.get(2))))
         .toList
       map = triples.groupBy(_.subj).view.mapValues(_.groupBy(_.obj).view.mapValues(_.map(_.pred)).toMap).toMap
-      result = {
-        implicit val blPredicateEncoder: Encoder[BiolinkPredicate] = Encoder.encodeString.contramap(predicate => predicate.shorthand)
-        implicit val blClassKeyEncoder: KeyEncoder[BiolinkClass] = (blClass: BiolinkClass) => blClass.shorthand
-        map.asJson.noSpaces
-      }
-    } yield result
+    } yield map
 
 }
