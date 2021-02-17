a05f01207751d1418d5c9f7737ff3e3e1ade4329
Jason Reilly <jdr0887@gmail.com>
2021-02-17 13:51:47 -0500

    initial commit

---
+++ src/test/scala/org/renci/cam/Biolink.scala
@@ -0,0 +1,28 @@
+package org.renci.cam
+
+import com.typesafe.scalalogging.LazyLogging
+import org.renci.cam.Scratch.{getClass, printEncoded, scratch}
+import zio.{Task, ZIO}
+import zio.test.Assertion.contains
+import zio.test.{DefaultRunnableSpec, assert, suite, testM}
+
+import scala.io.Source
+
+object Biolink extends DefaultRunnableSpec with LazyLogging {
+
+  val parseBiolinkYAML = suite("parse biolink yaml")(
+    testM("parse biolink yaml") {
+      for {
+        source <- Task.effect(Source.fromInputStream(getClass.getClassLoader.getResourceAsStream("biolink-model.yaml")).mkString)
+        json <- ZIO.fromEither(io.circe.yaml.parser.parse(source))
+        slotKeys <- ZIO.fromOption(json.hcursor.downField("slots").keys).orElseFail(throw new Exception("couldn't get slots"))
+        slots = slotKeys.map(a => a.replaceAll(" ", "_")).toList
+        prefixes <- ZIO.fromOption(json.hcursor.downField("prefixes").focus).orElseFail(throw new Exception("couldn't get slots"))
+        prefixesMap <- ZIO.fromEither(prefixes.as[Map[String, String]])
+      } yield assert(slots)(contains("related_to")) && assert(prefixesMap.keys)(contains("GOREL"))
+    }
+  )
+
+  def spec = suite("All tests")( parseBiolinkYAML)
+
+}
