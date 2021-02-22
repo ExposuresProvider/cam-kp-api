a7679506b787a6a3cc407cddd5ba0be2c4a5ba65
Jason Reilly <jdr0887@gmail.com>
2021-02-22 08:31:27 -0500

    adding tests

--- src/test/scala/org/renci/cam/test/BiolinkTest.scala
+++ src/test/scala/org/renci/cam/test/BiolinkTest.scala
@@ -1,27 +1,40 @@
 package org.renci.cam.test
 
 import com.typesafe.scalalogging.LazyLogging
-import zio.{Task, ZIO}
-import zio.test.Assertion.contains
-import zio.test.{DefaultRunnableSpec, assert, suite, testM}
-
-import scala.io.Source
+import org.renci.cam.Biolink
+import org.renci.cam.domain.{BiolinkClass, BiolinkPredicate}
+import zio.test.Assertion._
+import zio.test.{assert, suite, testM, DefaultRunnableSpec}
 
 object BiolinkTest extends DefaultRunnableSpec with LazyLogging {
 
-  val parseBiolinkYAML = suite("parse biolink yaml")(
-    testM("parse biolink yaml") {
+  val testLocalPrefixes = suite("localPrefixes")(
+    testM("test Biolink.localPrefixes") {
       for {
-        source <- Task.effect(Source.fromInputStream(getClass.getClassLoader.getResourceAsStream("biolink-model.yaml")).mkString)
-        json <- ZIO.fromEither(io.circe.yaml.parser.parse(source))
-        slotKeys <- ZIO.fromOption(json.hcursor.downField("slots").keys).orElseFail(throw new Exception("couldn't get slots"))
-        slots = slotKeys.map(a => a.replaceAll(" ", "_")).toList
-        prefixes <- ZIO.fromOption(json.hcursor.downField("prefixes").focus).orElseFail(throw new Exception("couldn't get slots"))
-        prefixesMap <- ZIO.fromEither(prefixes.as[Map[String, String]])
-      } yield assert(slots)(contains("related_to")) && assert(prefixesMap.keys)(contains("GOREL"))
+        prefixes <- Biolink.localPrefixes
+      } yield assert(prefixes)(isNonEmpty) && assert(prefixes.keys)(contains("sesame"))
     }
   )
 
-  def spec = suite("All tests")(parseBiolinkYAML)
+  val testGetBiolinkPrefixesFromFile = suite("getBiolinkPrefixesFromFile")(
+    testM("test Biolink.getBiolinkPrefixesFromFile") {
+      for {
+        prefixes <- Biolink.getBiolinkPrefixesFromFile
+      } yield assert(prefixes)(isNonEmpty) && assert(prefixes.keys.size)(isGreaterThan(190))
+    }
+  )
+
+  val testGetBiolinkPrefixesAndClassesAndPredicatesFromFile = suite("getBiolinkPrefixesAndClassesAndPredicatesFromFile")(
+    testM("test Biolink.getBiolinkPrefixesAndClassesAndPredicatesFromFile") {
+      for {
+        (prefixes, classes, predicates) <- Biolink.getBiolinkPrefixesAndClassesAndPredicatesFromFile
+      } yield assert(prefixes)(isNonEmpty) && assert(prefixes.keys)(contains("GOREL")) && assert(classes)(isNonEmpty) && assert(classes)(
+        contains(BiolinkClass("biological_entity"))) && assert(predicates)(isNonEmpty) && assert(predicates)(
+        contains(BiolinkPredicate("related_to")))
+    }
+  )
+
+  def spec =
+    suite("All tests")(testLocalPrefixes, testGetBiolinkPrefixesFromFile, testGetBiolinkPrefixesAndClassesAndPredicatesFromFile)
 
 }
