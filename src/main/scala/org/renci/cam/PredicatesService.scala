e7865b3bf2c2e87b4ee39341a9e6b937a9a1e787
Jason Reilly <jdr0887@gmail.com>
2021-02-23 11:35:12 -0500

    refactor to make more code available for unit testing

--- src/main/scala/org/renci/cam/PredicatesService.scala
+++ src/main/scala/org/renci/cam/PredicatesService.scala
@@ -1,16 +1,17 @@
 package org.renci.cam
 
-import java.io.{IOException, StringReader}
 import com.typesafe.scalalogging.LazyLogging
 import org.apache.commons.csv.CSVFormat
-import HttpClient.HttpClient
-import org.renci.cam.domain.{BiolinkClass, BiolinkPredicate}
 import org.renci.cam.Biolink.BiolinkData
+import org.renci.cam.HttpClient.HttpClient
+import org.renci.cam.domain.{BiolinkClass, BiolinkPredicate}
 import zio.ZIO.ZIOAutoCloseableOps
 import zio._
-import zio.blocking.{Blocking, effectBlockingIO}
+import zio.blocking._
 import zio.config.ZConfig
 
+import java.io.StringReader
+import java.nio.charset.StandardCharsets
 import scala.io.Source
 import scala.jdk.CollectionConverters._
 
@@ -18,21 +19,21 @@ object PredicatesService extends LazyLogging {
 
   case class Triple(subj: BiolinkClass, pred: BiolinkPredicate, obj: BiolinkClass)
 
-  def readPredicates: ZIO[Blocking, IOException, String] =
-    effectBlockingIO(Source.fromInputStream(getClass.getResourceAsStream("/predicates.csv"), "utf-8"))
-      .bracketAuto { source =>
-        effectBlockingIO(source.getLines().mkString("\n"))
-      }
-
   def run: RIO[ZConfig[AppConfig] with Blocking with HttpClient with Has[BiolinkData],
                Map[BiolinkClass, Map[BiolinkClass, List[BiolinkPredicate]]]] =
     for {
-      predicates <- readPredicates
-      records = CSVFormat.DEFAULT.parse(new StringReader(predicates)).getRecords
-      triples = records.asScala
-        .map(a => PredicatesService.Triple(BiolinkClass(a.get(0)), BiolinkPredicate(a.get(1)), BiolinkClass(a.get(2))))
-        .toList
-      map = triples.groupBy(_.subj).view.mapValues(_.groupBy(_.obj).view.mapValues(_.map(_.pred)).toMap).toMap
-    } yield map
+      triples <- readPredicates
+    } yield triples.groupBy(_.subj).view.mapValues(_.groupBy(_.obj).view.mapValues(_.map(_.pred)).toMap).toMap
+
+  def readPredicates: ZIO[Blocking, Throwable, List[Triple]] = for {
+    predicates <- effectBlockingIO(Source.fromInputStream(getClass.getResourceAsStream("/predicates.csv"), StandardCharsets.UTF_8.name()))
+      .bracketAuto { source =>
+        effectBlockingIO(source.getLines().mkString("\n"))
+      }
+    records <- Task.effect(CSVFormat.DEFAULT.parse(new StringReader(predicates)).getRecords)
+    triples = records.asScala
+      .map(a => Triple(BiolinkClass(a.get(0)), BiolinkPredicate(a.get(1)), BiolinkClass(a.get(2))))
+      .toList
+  } yield triples
 
 }
