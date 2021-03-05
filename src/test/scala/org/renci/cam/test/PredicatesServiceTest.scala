88eed22d72f7fbce002449979d580812311d1f5a
Jason Reilly <jdr0887@gmail.com>
2021-03-05 09:37:09 -0500

    fixing imports

--- src/test/scala/org/renci/cam/test/PredicatesServiceTest.scala
+++ src/test/scala/org/renci/cam/test/PredicatesServiceTest.scala
@@ -1,10 +1,13 @@
 package org.renci.cam.test
 
 import com.typesafe.scalalogging.LazyLogging
+import io.circe.{Decoder, Encoder, parser}
 import org.renci.cam._
 import org.renci.cam.domain._
 import zio.test.Assertion._
 import zio.test._
+
+import java.nio.file.{Files, Paths}
 
 object PredicatesServiceTest extends DefaultRunnableSpec with LazyLogging {
 
