775a900b9d9cec719b06b5e0785a4ed6ba600c47
Jason Reilly <jdr0887@gmail.com>
2021-03-05 08:45:30 -0500

    rename PullBiolinkTypeMapTest & PredicateServiceTest

--- src/it/scala/org/renci/cam/it/BiolinkTest.scala
+++ src/it/scala/org/renci/cam/it/BiolinkTest.scala
@@ -1,7 +1,6 @@
 package org.renci.cam.it
 
 import java.nio.file.{Files, Paths}
-
 import io.circe._
 import io.circe.parser._
 import org.http4s._
@@ -11,20 +10,25 @@ import zio._
 import zio.interop.catz._
 import zio.test.Assertion._
 import zio.test._
+
 import java.nio.file.{Files, Paths}
 import org.renci.cam._
+import zio.blocking.Blocking
 
 object PullBiolinkTypeMapTest extends DefaultRunnableSpec {
+
+  val camkpapiLayer = HttpClient.makeHttpClientLayer
+  val testLayer = zio.test.environment.testEnvironment ++ camkpapiLayer
 
   val decoder = Decoder.decodeMap(KeyDecoder.decodeKeyString, Decoder.decodeString)
 
   val suite1 = suite("PullBiolinkTypeMapTestSpec")(
     testM("pull") {
-      val uri = uri"https://biolink.github.io/biolink-model/context.jsonld"
-      val request =
-        Request[Task](Method.GET, uri).withHeaders(Accept(MediaType.application.`ld+json`), `Content-Type`(MediaType.application.`ld+json`))
-      for {
-        response <- HttpClient.makeHttpClient.use(_.expect[String](request))
+      val testCase = for {
+        httpClient <- HttpClient.client
+        uri = uri"https://biolink.github.io/biolink-model/context.jsonld"
+        request = Request[Task](Method.GET, uri).withHeaders(Accept(MediaType.application.`ld+json`), `Content-Type`(MediaType.application.`ld+json`))
+        response <- httpClient.expect[String](request)
         parsed <- ZIO.fromEither(parse(response))
         contextJson <- ZIO.fromOption(parsed.hcursor.downField("@context").focus)
         filteredJson = contextJson.deepDropNullValues.mapObject(f =>
@@ -32,10 +36,11 @@ object PullBiolinkTypeMapTest extends DefaultRunnableSpec {
         _ <- ZIO.effect(Files.write(Paths.get("src/main/resources/prefixes.json"), filteredJson.toString().getBytes))
         map <- ZIO.fromEither(decoder.decodeJson(filteredJson))
         _ = ZIO.effect(println(map.toString))
-      } yield assert(response)(isNonEmptyString)
-    } //@@ ignore
+      } yield assert(map.keys)(contains())
+      testCase.provideCustomLayer(testLayer)
+    }
   )
 
-  def spec = suite("All tests")(suite1)
+  def spec = suite("PullBiolinkTypeMap tests")(suite1)
 
 }
---
+++ src/it/scala/org/renci/cam/it/PredicatesServiceTest.scala
@@ -0,0 +1,53 @@
+package org.renci.cam.it
+
+import cats.instances.map
+import com.dimafeng.testcontainers.GenericContainer
+import org.http4s._
+import org.http4s.dsl.request
+import org.http4s.headers.`Content-Type`
+import org.http4s.implicits._
+import org.renci.cam._
+import org.renci.cam.domain.{BiolinkClass, BiolinkPredicate, IRI, TRAPIQuery}
+import zio.{Task, ZIO}
+import zio.blocking.Blocking
+import zio.interop.catz._
+import zio.test.Assertion._
+import zio.test._
+import io.circe.generic.auto._
+import io.circe.generic.semiauto._
+import io.circe.parser._
+import io.circe.syntax._
+import io.circe.{Decoder, Encoder, HCursor, Json, KeyDecoder, KeyEncoder, parser}
+
+object PredicateServiceTest extends DefaultRunnableSpec {
+
+  val camkpapiTestLayer = Blocking.live >>> TestContainer.camkpapi
+  val camkpapiLayer = HttpClient.makeHttpClientLayer >+> Biolink.makeUtilitiesLayer
+  val testLayer = zio.test.environment.testEnvironment ++ camkpapiTestLayer ++ camkpapiLayer
+
+  val predicatesServiceTest = suite("PredicatesService test")(
+    testM("test parsing predicates and for the existence of IndividualOrganism") {
+      val testCase = for {
+        httpClient <- HttpClient.client
+        biolinkData <- Biolink.biolinkData
+        request = Request[Task](Method.GET, uri"http://127.0.0.1:8080/predicates").withHeaders(`Content-Type`(MediaType.application.json))
+        response <- httpClient.expect[String](request)
+      } yield {
+        println("response: " + response)
+
+        implicit val biolinkClassKeyDecoder = Implicits.biolinkClassKeyDecoder(biolinkData.classes)
+        implicit val biolinkClassKeyEncoder = Implicits.biolinkClassKeyEncoder
+        implicit val biolinkPredicateEncoder: Encoder[BiolinkPredicate] = Implicits.biolinkPredicateEncoder
+        implicit val biolinkPredicateDecoder: Decoder[BiolinkPredicate] = Implicits.biolinkPredicateDecoder(biolinkData.predicates)
+
+        val parsed = parser.parse(response).toOption.get
+        val map = parsed.as[Map[BiolinkClass, Map[BiolinkClass, List[BiolinkPredicate]]]]
+        assert(response)(isNonEmptyString) && assert(map.toOption.get.keys)(contains(BiolinkClass("IndividualOrganism")))
+      }
+      testCase.provideCustomLayer(testLayer)
+    }
+  )
+
+  def spec = suite("PredicateService tests")(predicatesServiceTest)
+
+}
