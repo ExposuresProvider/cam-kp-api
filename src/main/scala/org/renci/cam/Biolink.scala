ca80bd5091f104f1f8bbc8e4274b344d5b5f8d4b
Jason Reilly <jdr0887@gmail.com>
2021-05-20 10:32:07 -0400

    only pulling data from local biolink-data.json file

--- src/main/scala/org/renci/cam/Biolink.scala
+++ src/main/scala/org/renci/cam/Biolink.scala
@@ -1,6 +1,8 @@
 package org.renci.cam
 
-import io.circe.Json
+import com.typesafe.scalalogging.LazyLogging
+import io.circe._
+import io.circe.generic.auto._
 import org.apache.commons.lang3.StringUtils
 import org.http4s.circe._
 import org.http4s.headers.Accept
@@ -11,28 +13,39 @@ import org.renci.cam.domain.{BiolinkClass, BiolinkPredicate}
 import zio._
 import zio.interop.catz._
 
+import scala.collection.immutable.ListMap
 import scala.io.Source
 
-object Biolink {
+object Biolink extends LazyLogging {
 
-  final case class BiolinkData(prefixes: Map[String, String], classes: List[BiolinkClass], predicates: List[BiolinkPredicate])
+  final case class BiolinkData(version: String,
+                               prefixes: Map[String, String],
+                               classes: List[BiolinkClass],
+                               predicates: List[BiolinkPredicate])
 
   def makeUtilitiesLayer: ZLayer[HttpClient, Throwable, Has[BiolinkData]] = getBiolinkData.toLayer
 
   val biolinkData: URIO[Has[BiolinkData], BiolinkData] = ZIO.service
 
-  def getBiolinkData: ZIO[HttpClient, Throwable, BiolinkData] =
+  def getBiolinkData: ZIO[HttpClient, Throwable, BiolinkData] = {
+    val sourceManaged = for {
+      fileStream <- Managed.fromAutoCloseable(Task.effect(getClass.getClassLoader.getResourceAsStream("biolink-data.json")))
+      source <- Managed.fromAutoCloseable(Task.effect(Source.fromInputStream(fileStream)))
+    } yield source
+
+    implicit val biolinkClassDecoder: Decoder[BiolinkClass] = Decoder.decodeString.map(s => BiolinkClass(s))
+    implicit val biolinkPredicateDecoder: Decoder[BiolinkPredicate] = Decoder.decodeString.map(s => BiolinkPredicate(s))
+
     for {
-      (biolinkPrefixes, classes, predicates) <- getBiolinkPrefixesAndClassesAndPredicatesFromFile
-      legacy <- localPrefixes
-      prefixes <- getBiolinkPrefixesFromURL.orElse(getBiolinkPrefixesFromFile)
-      prefixOverrides <- getPrefixOverrides
-      combined = legacy ++ biolinkPrefixes ++ prefixes ++ prefixOverrides
-    } yield BiolinkData(combined, classes, predicates)
+      biolinkDataString <- sourceManaged.use(source => ZIO.effect(source.getLines().mkString))
+      biolinkDataJson <- Task.effect(io.circe.parser.parse(biolinkDataString).getOrElse(Json.Null))
+      biolinkData <- ZIO.fromEither(biolinkDataJson.as[BiolinkData])
+    } yield biolinkData
+  }
 
   def getPrefixOverrides: ZIO[Any, Throwable, Map[String, String]] = {
     val sourceManaged = for {
-      fileStream <- Managed.fromAutoCloseable(Task.effect(getClass.getClassLoader.getResourceAsStream("prefixesOverrides.json")))
+      fileStream <- Managed.fromAutoCloseable(Task.effect(getClass.getClassLoader.getResourceAsStream("prefixes.json")))
       source <- Managed.fromAutoCloseable(Task.effect(Source.fromInputStream(fileStream)))
     } yield source
     for {
@@ -42,7 +55,7 @@ object Biolink {
     } yield mappings
   }
 
-  def getBiolinkPrefixesFromURL: ZIO[HttpClient, Throwable, Map[String, String]] =
+  def getBiolinkPrefixesFromContext: ZIO[HttpClient, Throwable, Map[String, String]] =
     for {
       httpClient <- HttpClient.client
       uri = uri"https://biolink.github.io/biolink-model/context.jsonld"
@@ -66,47 +79,24 @@ object Biolink {
       map = firstPass ++ secondPass
     } yield map
 
-  def getBiolinkPrefixesFromFile: ZIO[Any, Throwable, Map[String, String]] = {
-    val sourceManaged = for {
-      fileStream <- Managed.fromAutoCloseable(Task.effect(getClass.getClassLoader.getResourceAsStream("prefixes.json")))
-      source <- Managed.fromAutoCloseable(Task.effect(Source.fromInputStream(fileStream)))
-    } yield source
+  def getBiolinkPrefixesAndClassesAndPredicatesFromModel
+    : ZIO[HttpClient, Throwable, (Map[String, String], List[BiolinkClass], List[BiolinkPredicate], String)] =
     for {
-      prefixesStr <- sourceManaged.use(source => ZIO.effect(source.getLines().mkString))
-      prefixesJson <- Task.effect(io.circe.parser.parse(prefixesStr).getOrElse(Json.Null))
-      curies <- ZIO.fromEither(prefixesJson.as[Map[String, String]])
-    } yield curies
-  }
-
-  def localPrefixes: ZIO[Any, Throwable, Map[String, String]] = {
-    val sourceManaged = for {
-      fileStream <- Managed.fromAutoCloseable(Task.effect(getClass.getClassLoader.getResourceAsStream("legacy_prefixes.json")))
-      source <- Managed.fromAutoCloseable(Task.effect(Source.fromInputStream(fileStream)))
-    } yield source
-    for {
-      prefixesStr <- sourceManaged.use(source => ZIO.effect(source.getLines().mkString))
-      prefixesJson <- ZIO.fromEither(io.circe.parser.parse(prefixesStr))
-      prefixes <- ZIO.fromEither(prefixesJson.as[Map[String, String]])
-    } yield prefixes
-  }
-
-  // originates from https://biolink.github.io/biolink-model/biolink-model.yaml
-  def getBiolinkPrefixesAndClassesAndPredicatesFromFile
-    : ZIO[Any, Throwable, (Map[String, String], List[BiolinkClass], List[BiolinkPredicate])] = {
-    val sourceManaged = for {
-      source <-
-        Managed.fromAutoCloseable(Task.effect(Source.fromInputStream(getClass.getClassLoader.getResourceAsStream("biolink-model.yaml"))))
-    } yield source
-    for {
-      slotsStr <- sourceManaged.use(source => ZIO.effect(source.mkString))
-      json <- ZIO.fromEither(io.circe.yaml.parser.parse(slotsStr))
+      httpClient <- HttpClient.client
+      uri = uri"https://biolink.github.io/biolink-model/biolink-model.yaml"
+      _ = logger.info("here 1")
+      request = Request[Task](Method.GET, uri).withHeaders(Accept(MediaType.application.`raml+yaml`))
+      biolinkModelYaml <- httpClient.expect[String](request)
+      json <- ZIO.fromEither(io.circe.yaml.parser.parse(biolinkModelYaml))
+      _ = logger.info("here 2")
       classesKeys <- ZIO.fromOption(json.hcursor.downField("classes").keys).orElseFail(throw new Exception("couldn't get classes"))
       classes = classesKeys.map(a => a.split(" ").toList.map(a => StringUtils.capitalize(a)).mkString).map(a => BiolinkClass(a)).toList
       predicateKeys <- ZIO.fromOption(json.hcursor.downField("slots").keys).orElseFail(throw new Exception("couldn't get slots"))
       predicates = predicateKeys.map(a => BiolinkPredicate(a.replaceAll(",", "_").replaceAll(" ", "_"))).toList
       prefixes <- ZIO.fromOption(json.hcursor.downField("prefixes").focus).orElseFail(throw new Exception("couldn't get prefixes"))
       prefixesMap <- ZIO.fromEither(prefixes.as[Map[String, String]])
-    } yield (prefixesMap, classes, predicates)
-  }
+      versionJson <- ZIO.fromOption(json.hcursor.downField("version").focus).orElseFail(throw new Exception("couldn't get version"))
+      version <- ZIO.fromEither(versionJson.as[String])
+    } yield (prefixesMap, classes, predicates, version)
 
 }
