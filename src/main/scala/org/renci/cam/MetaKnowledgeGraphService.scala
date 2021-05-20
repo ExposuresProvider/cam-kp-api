874ac2c051c2bffd7ba0b0c6d28aa5e57dd5a6d0
Jason Reilly <jdr0887@gmail.com>
2021-05-20 10:32:37 -0400

    cleaning and using keys instead of values

--- src/main/scala/org/renci/cam/MetaKnowledgeGraphService.scala
+++ src/main/scala/org/renci/cam/MetaKnowledgeGraphService.scala
@@ -17,8 +17,6 @@ import scala.jdk.CollectionConverters._
 
 object MetaKnowledgeGraphService extends LazyLogging {
 
-//  case class Triple(subj: BiolinkClass, pred: BiolinkPredicate, obj: BiolinkClass)
-
   def run: RIO[ZConfig[AppConfig] with Blocking with HttpClient with Has[BiolinkData], MetaKnowledgeGraph] = readPredicates
 
   def readPredicates: ZIO[Blocking with HttpClient with Has[BiolinkData], Throwable, MetaKnowledgeGraph] = for {
@@ -28,14 +26,9 @@ object MetaKnowledgeGraphService extends LazyLogging {
         effectBlockingIO(source.getLines().mkString("\n"))
       }
     records <- Task.effect(CSVFormat.DEFAULT.parse(new StringReader(predicates)).getRecords)
-    id_prefixes = biolinkData.prefixes.values.toList
+    id_prefixes = biolinkData.prefixes.keys.toList
     metaNode = MetaNode(id_prefixes)
     metaEdges = records.asScala.map(a => MetaEdge(BiolinkClass(a.get(0)), BiolinkPredicate(a.get(1)), BiolinkClass(a.get(2)), None)).toList
   } yield MetaKnowledgeGraph(metaNode, metaEdges)
-
-//    triples = records.asScala
-//      .map(a => Triple(BiolinkClass(a.get(0)), BiolinkPredicate(a.get(1)), BiolinkClass(a.get(2))))
-//      .toList
-//  } yield triples.groupBy(_.subj).view.mapValues(_.groupBy(_.obj).view.mapValues(_.map(_.pred)).toMap).toMap
 
 }
