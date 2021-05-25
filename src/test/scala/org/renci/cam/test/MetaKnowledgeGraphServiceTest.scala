006062f1067c0850454a199ff53321f28001e6d6
Jason Reilly <jdr0887@gmail.com>
2021-05-25 15:57:40 -0400

    adding testGetEdges & testGetNodes

--- src/test/scala/org/renci/cam/test/MetaKnowledgeGraphServiceTest.scala
+++ src/test/scala/org/renci/cam/test/MetaKnowledgeGraphServiceTest.scala
@@ -1,18 +1,29 @@
 package org.renci.cam.test
 
 import com.typesafe.scalalogging.LazyLogging
+import org.renci.cam.MetaKnowledgeGraphService
+import zio.test.Assertion.isNonEmpty
 import zio.test._
 
 object MetaKnowledgeGraphServiceTest extends DefaultRunnableSpec with LazyLogging {
 
-//  val testReadMetaKnowledgeGraph = suite("readMetaKnowledgeGraph")(
-//    testM("test MetaKnowledgeGraphService.readMetaKnowledgeGraph") {
-//      for {
-//        predicates <- MetaKnowledgeGraphService.readPredicates
-//      } yield assert(predicates)(isNonEmpty) && assert(predicates.keys)(contains(BiolinkClass("MacromolecularMachine")))
-//    }
-//  )
+  val testGetEdges = suite("MetaKnowledgeGraphService.getEdges")(
+    testM("test MetaKnowledgeGraphService.getEdges") {
+      for {
+        edges <- MetaKnowledgeGraphService.getEdges
+      } yield assert(edges)(isNonEmpty)
+    }
+  )
 
-  def spec = suite("MetaKnowledgeGraphService tests")( /*testReadMetaKnowledgeGraph*/ )
+  val testGetNodes = suite("MetaKnowledgeGraphService.getNodes")(
+    testM("test MetaKnowledgeGraphService.getNodes") {
+      for {
+        nodes <- MetaKnowledgeGraphService.getNodes
+//        _ = logger.info("nodes: {}", nodes)
+      } yield assert(nodes)(isNonEmpty)
+    }
+  )
+
+  def spec = suite("MetaKnowledgeGraphService tests")( testGetEdges, testGetNodes )
 
 }
