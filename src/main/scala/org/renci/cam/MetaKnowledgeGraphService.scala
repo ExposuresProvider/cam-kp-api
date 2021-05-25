dad40ce56eb94b69b38e0c917cace89aa3f710aa
Jason Reilly <jdr0887@gmail.com>
2021-05-25 15:53:52 -0400

    adding getEdges & getNodes

--- src/main/scala/org/renci/cam/MetaKnowledgeGraphService.scala
+++ src/main/scala/org/renci/cam/MetaKnowledgeGraphService.scala
@@ -17,18 +17,31 @@ import scala.jdk.CollectionConverters._
 
 object MetaKnowledgeGraphService extends LazyLogging {
 
-  def run: RIO[ZConfig[AppConfig] with Blocking with HttpClient with Has[BiolinkData], MetaKnowledgeGraph] = readPredicates
+  def run: RIO[ZConfig[AppConfig] with Blocking with HttpClient with Has[BiolinkData], MetaKnowledgeGraph] = getMetaKnowledgeGraph
 
-  def readPredicates: ZIO[Blocking with HttpClient with Has[BiolinkData], Throwable, MetaKnowledgeGraph] = for {
-    biolinkData <- Biolink.biolinkData
+  def getMetaKnowledgeGraph: ZIO[Blocking with HttpClient with Has[BiolinkData], Throwable, MetaKnowledgeGraph] = for {
+//    biolinkData <- Biolink.biolinkData
+    metaNodes <- getNodes
+    metaEdges <- getEdges
+  } yield MetaKnowledgeGraph(metaNodes, metaEdges)
+
+  def getEdges: ZIO[Blocking, Throwable, List[MetaEdge]] = for {
     predicates <- effectBlockingIO(Source.fromInputStream(getClass.getResourceAsStream("/predicates.csv"), StandardCharsets.UTF_8.name()))
       .bracketAuto { source =>
         effectBlockingIO(source.getLines().mkString("\n"))
       }
-    records <- Task.effect(CSVFormat.DEFAULT.parse(new StringReader(predicates)).getRecords)
-    id_prefixes = biolinkData.prefixes.keys.toList
-    metaNode = MetaNode(id_prefixes)
-    metaEdges = records.asScala.map(a => MetaEdge(BiolinkClass(a.get(0)), BiolinkPredicate(a.get(1)), BiolinkClass(a.get(2)), None)).toList
-  } yield MetaKnowledgeGraph(metaNode, metaEdges)
+    predicateRecords <- Task.effect(CSVFormat.DEFAULT.parse(new StringReader(predicates)).getRecords)
+    metaEdges = predicateRecords.asScala.map(a => MetaEdge(BiolinkClass(a.get(0)), BiolinkPredicate(a.get(1)), BiolinkClass(a.get(2)), None)).toList
+  } yield metaEdges
+
+  def getNodes: ZIO[Blocking, Throwable, List[MetaNode]] = for {
+    mkgNodesData <- effectBlockingIO(Source.fromInputStream(getClass.getResourceAsStream("/mkg-nodes.csv"), StandardCharsets.UTF_8.name()))
+      .bracketAuto { source =>
+        effectBlockingIO(source.getLines().mkString("\n"))
+      }
+    mkgNodeRecords <- Task.effect(CSVFormat.DEFAULT.parse(new StringReader(mkgNodesData)).getRecords)
+    mkgMap = mkgNodeRecords.asScala.groupBy(_.get(0)).view.mapValues(_.map(_.get(1)).toList).toMap
+    metaNodes = mkgMap.map(a => MetaNode(BiolinkClass(a._1), a._2)).toList
+  } yield metaNodes
 
 }
