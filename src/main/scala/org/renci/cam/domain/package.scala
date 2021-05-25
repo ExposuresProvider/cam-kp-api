4c7baec35d1e9e922d2e6b615c495263a0104e8e
Jason Reilly <jdr0887@gmail.com>
2021-05-25 16:25:33 -0400

    fixing model

--- src/main/scala/org/renci/cam/domain/package.scala
+++ src/main/scala/org/renci/cam/domain/package.scala
@@ -158,10 +158,10 @@ package object domain {
 
   final case class LogEntry(timestamp: Option[String], level: Option[String], code: Option[String], message: Option[String])
 
-  final case class MetaNode(biolinkClass: BiolinkClass, id_prefixes: List[String])
+//  final case class MetaNode(biolinkClass: BiolinkClass, id_prefixes: List[String])
 
   final case class MetaEdge(subject: BiolinkClass, predicate: BiolinkPredicate, `object`: BiolinkClass, relations: Option[List[String]])
 
-  final case class MetaKnowledgeGraph(nodes: List[MetaNode], edges: List[MetaEdge])
+  final case class MetaKnowledgeGraph(nodes: Map[BiolinkClass, Map[String, List[String]]], edges: List[MetaEdge])
 
 }
