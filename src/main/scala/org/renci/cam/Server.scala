97e76a0dcb1140267e7ae2581aec25de72108380
Jason Reilly <jdr0887@gmail.com>
2021-08-23 09:47:47 -0400

    cleaning

--- src/main/scala/org/renci/cam/Server.scala
+++ src/main/scala/org/renci/cam/Server.scala
@@ -50,8 +50,8 @@ object Server extends App with LazyLogging {
       .out(
         {
 
-//          implicit val bcKeyDecoder: KeyDecoder[BiolinkClass] = Implicits.biolinkClassKeyDecoder(biolinkData.classes)
-//          implicit val bcKeyEncoder: KeyEncoder[BiolinkClass] = Implicits.biolinkClassKeyEncoder
+          implicit val bcKeyDecoder: KeyDecoder[BiolinkClass] = Implicits.biolinkClassKeyDecoder(biolinkData.classes)
+          implicit val bcKeyEncoder: KeyEncoder[BiolinkClass] = Implicits.biolinkClassKeyEncoder
 
 //          implicit val iriDecoder: Decoder[IRI] = Implicits.iriDecoder(biolinkData.prefixes)
 //          implicit val iriEncoder: Encoder[IRI] = Implicits.iriEncoder(biolinkData.prefixes)
@@ -61,9 +61,6 @@ object Server extends App with LazyLogging {
 //
 //          implicit val blPredicateDecoder: Decoder[BiolinkPredicate] = Implicits.biolinkPredicateDecoder(biolinkData.predicates)
 //          implicit val blPredicateEncoder: Encoder[BiolinkPredicate] = Implicits.biolinkPredicateEncoder(biolinkData.prefixes)
-
-//          implicit val metaNodeKeyEncoder: KeyEncoder[MetaNode] = Implicits.metaNodeKeyEncoder
-          implicit val metaNodeEncoder: Encoder[MetaNode] = Implicits.metaNodeEncoder
 
           jsonBody[MetaKnowledgeGraph]
         }
