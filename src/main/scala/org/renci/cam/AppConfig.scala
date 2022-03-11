25171501228a53354ed48a431ce17b3bd6dda415
Jason Reilly <jdr0887@gmail.com>
2022-03-11 15:38:49 -0500

    removing sparqlEndpoint:

--- src/main/scala/org/renci/cam/AppConfig.scala
+++ src/main/scala/org/renci/cam/AppConfig.scala
@@ -4,12 +4,9 @@ import org.http4s.Uri
 import zio.config._
 import zio.config.magnolia.DeriveConfigDescriptor.{Descriptor, descriptor, _}
 
-final case class AppConfig(host: String, port: Int, location: String, sparqlEndpoint: Uri, trapiVersion: String, maturity: String)
+final case class AppConfig(host: String, port: Int, location: String, trapiVersion: String, maturity: String)
 
 object AppConfig {
-
-  implicit val uriDescriptor: Descriptor[Uri] =
-    Descriptor[String].transformEitherLeft(Uri.fromString)(_.toString)(_.getMessage)
 
   val config: ConfigDescriptor[AppConfig] = descriptor[AppConfig].mapKey(toKebabCase)
 
