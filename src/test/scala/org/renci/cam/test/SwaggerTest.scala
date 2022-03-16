22828ecb50317682fc2668e3d583dcddae433bd1
Gaurav Vaidya <gaurav@ggvaidya.com>
2022-03-16 11:36:55 -0400

    Incomplete commit for HTTP testing.

--- src/test/scala/org/renci/cam/test/SwaggerTest.scala
+++ src/test/scala/org/renci/cam/test/SwaggerTest.scala
@@ -1,31 +1,31 @@
 package org.renci.cam.test
 
-import org.http4s.Uri
+import org.http4s.LiteralSyntaxMacros.uri
+import org.http4s.{Method, Request, Uri}
 import org.http4s.implicits._
-import org.renci.cam.{AppConfig, HttpClient}
-import zio.ZIO
+import org.renci.cam.HttpClient.{HttpClient, makeHttpClient}
+import org.renci.cam.{AppConfig, HttpClient, QueryService, Server}
+import zio.{RIO, ZIO}
 import zio.ZIO.debug
 import zio.config.getConfig
 import zio.interop.catz._
-import zio.test.Assertion.containsString
+import zio.test.Assertion.{containsString, forall}
 import zio.test._
+import org.http4s.dsl.io._
+import org.renci.cam.Server.EndpointEnv
 import zio.test.environment.testEnvironment
+
+import java.net.URI
 
 object SwaggerTest extends DefaultRunnableSpec {
   val appConfig = getConfig[AppConfig]
 
   val testLayer = (testEnvironment ++ HttpClient.makeHttpClientLayer).mapError(TestFailure.die)
 
-  def httpGetAsString(uri: Uri): ZIO[HttpClient.HttpClient, Throwable, String] = for {
-    httpClient <- HttpClient.client
-    response <- httpClient.expect[String](uri)
-  } yield response
-
   val testSwaggerEndpoints = suite("Test the Swagger endpoints")(
     testM("Test whether we can access the Swagger endpoint") {
-      val response = httpGetAsString(uri"http://127.0.0.1:8080/docs/")
-
-      assertM(response)(containsString("help"))
+      // val q = QueryService.run(20, false, )
+      val r = makeHttpClient.asService
     }
   )
 
