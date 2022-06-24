4f350b642eee68edcaaa34a979eeea96c2630ac1
Gaurav Vaidya <gaurav@renci.org>
2022-06-24 18:06:04 -0400

    Improved LimitTest.

--- src/test/scala/org/renci/cam/test/LimitTest.scala
+++ src/test/scala/org/renci/cam/test/LimitTest.scala
@@ -1,24 +1,13 @@
 package org.renci.cam.test
 
 import com.typesafe.scalalogging.LazyLogging
-import io.circe.syntax._
-import io.circe.{Encoder, Json}
-import org.apache.jena.query.{Query, QuerySolution}
-import org.renci.cam.HttpClient.HttpClient
-import org.renci.cam.SPARQLQueryExecutor.SPARQLCache
-import org.renci.cam.{AppConfig, Biolink, HttpClient, QueryService, SPARQLQueryExecutor}
 import org.renci.cam.domain._
-import zio.ExecutionStrategy.Sequential
-import zio.blocking.Blocking
-import zio.cache.Cache
-import zio.config.{ConfigModule, ZConfig}
-import zio.{Layer, RIO, Runtime, ZIO, ZLayer}
+import org.renci.cam._
+import zio.Layer
+import zio.config.ZConfig
 import zio.config.typesafe.TypesafeConfig
 import zio.stream.ZStream
-import zio.test.Assertion._
-import zio.test.TestAspect.{debug, ignore}
 import zio.test._
-import zio.test.environment.testEnvironment
 
 /**
  * Tests whether queries work as expected with limits, i.e. given a query, can we increase limits arbitrarily
@@ -26,8 +15,16 @@ import zio.test.environment.testEnvironment
  */
 object LimitTest extends DefaultRunnableSpec with LazyLogging {
 
+  /**
+   * For a query with a certain number of expected results, this code block creates a number of tests that test
+   * whether setting `limit` from 1..50 return the correct number of expected results.
+   */
   val testQueryWithExpectedResults = {
-    val queryGraphExpectedResults = 30 // expected results as of 2022-may-18
+    // Expected results as of 2022-may-18 for the test query
+    val queryGraphExpectedResults = 30
+    // The limits to test -- this should be set up to go neatly within
+    val limitsToTest = Seq(1, 2, 3, 4, 5, 10, 20, 30, 40, 50)
+    // The test query: pyruvate related_to $[NamedThing]
     val testQueryGraph = TRAPIQueryGraph(
       Map(
         "n0" -> TRAPIQueryNode(Some(List(IRI("http://purl.obolibrary.org/obo/CHEBI_15361"))), None, None),
@@ -38,31 +35,28 @@ object LimitTest extends DefaultRunnableSpec with LazyLogging {
       )
     )
 
-    val limitsToTest = Seq(1, 2, 3, 4, 5, 10, 20, 30, 40, 50)
     suiteM("testQueryWithExpectedResults") {
       ZStream.fromIterable(limitsToTest)
         .map(limit => testM(s"Test query with limit of ${limit} expecting ${queryGraphExpectedResults} results") {
           for {
             message <- QueryService.run(limit, false, testQueryGraph)
-            _ = println(s"Retrieved ${message.results.get.size} results when limit=${limit}")
+            _ = logger.info(s"Retrieved ${message.results.get.size} results when limit=${limit}")
             results = message.results.get
           } yield {
-            /*
-            logger.info(s"Knowledge graph:")
-            logger.info(s" - Nodes:")
+            logger.debug(s"Knowledge graph:")
+            logger.debug(s" - Nodes:")
             message.knowledge_graph.foreach(_.nodes.foreach(node =>
-              logger.info(s"   - ${node._1}: ${node._2}")
+              logger.debug(s"   - ${node._1}: ${node._2}")
             ))
-            logger.info(s" - Edges:")
+            logger.debug(s" - Edges:")
             message.knowledge_graph.foreach(_.edges.foreach(edge =>
-              logger.info(s"   - ${edge._1}: ${edge._2}")
+              logger.debug(s"   - ${edge._1}: ${edge._2}")
             ))
-
-            logger.info(s"Results:")
+            logger.debug(s"Results:")
             for ((r, index) <- results.zipWithIndex) {
-              logger.info(s" - [${index + 1}] ${r}")
+              logger.debug(s" - [${index + 1}] ${r}")
             }
-             */
+
             assert(results.size)(Assertion.isGreaterThan(0)) &&
               assert(results.size)(Assertion.equalTo(Math.min(queryGraphExpectedResults, limit)))
           }
@@ -73,7 +67,7 @@ object LimitTest extends DefaultRunnableSpec with LazyLogging {
   val configLayer: Layer[Throwable, ZConfig[AppConfig]] = TypesafeConfig.fromDefaultLoader(AppConfig.config)
   val testLayer = HttpClient.makeHttpClientLayer ++ Biolink.makeUtilitiesLayer ++ configLayer >+> SPARQLQueryExecutor.makeCache.toLayer
 
-  def spec = suite("Limit tests")(
+  def spec: Spec[environment.TestEnvironment, TestFailure[Throwable], TestSuccess] = suite("Limit tests")(
     testQueryWithExpectedResults
   ).provideCustomLayer(testLayer.mapError(TestFailure.die))
 }
