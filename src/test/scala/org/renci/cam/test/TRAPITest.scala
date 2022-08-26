7dc91681b6ba96b4ff9be35d51e708f1a1496170
Gaurav Vaidya <gaurav@ggvaidya.com>
2022-08-25 23:29:37 -0400

    Removed QNode/QEdge additional property test.
    This test is now implemented in PR #560.

--- src/test/scala/org/renci/cam/test/TRAPITest.scala
+++ src/test/scala/org/renci/cam/test/TRAPITest.scala
@@ -4,7 +4,7 @@ import com.typesafe.scalalogging.LazyLogging
 import io.circe._
 import io.circe.generic.auto._
 import io.circe.generic.semiauto._
-import org.http4s.headers.{Accept, `Content-Type`}
+import org.http4s.headers.{`Content-Type`, Accept}
 import org.http4s.{EntityDecoder, MediaType, Method, Request, Uri}
 import org.renci.cam.Biolink.biolinkData
 import org.renci.cam._
@@ -17,10 +17,9 @@ import zio.{Layer, ZIO}
 
 object TRAPITest extends DefaultRunnableSpec with LazyLogging {
 
-  /**
-   * If CAM-KP-API receives a attribute constraint that it does not support, it MUST respond with an error Code
-   * "UnsupportedAttributeConstraint".
-   */
+  /** If CAM-KP-API receives a attribute constraint that it does not support, it MUST respond with an error Code
+    * "UnsupportedAttributeConstraint".
+    */
   val testUnsupportedAttributeConstraints = suite("testUnsupportedAttributeConstraints") {
     // Examples taken from https://github.com/NCATSTranslator/ReasonerAPI/blob/1e0795a1c4ff5bcac3ccd5f188fdc09ec6bd27c3/ImplementationRules.md#specifying-permitted-and-excluded-kps-to-an-ara
     val query = """{
@@ -80,10 +79,10 @@ object TRAPITest extends DefaultRunnableSpec with LazyLogging {
         )
 
         logs = trapiResponse.logs
-        logsWithUnsupportedAttributeConstraint = logs.getOrElse(List()).filter({
+        logsWithUnsupportedAttributeConstraint = logs.getOrElse(List()).filter {
           case LogEntry(_, Some("Error"), Some("UnsupportedAttributeConstraint"), _) => true
-          case _ => false
-        })
+          case _                                                                     => false
+        }
       } yield assert(response.status)(Assertion.hasField("isSuccess", _.isSuccess, Assertion.isTrue)) &&
         assert(content)(Assertion.isNonEmptyString) &&
         // Should return an UnsupportedAttributeConstraint as the status ...
@@ -94,12 +93,11 @@ object TRAPITest extends DefaultRunnableSpec with LazyLogging {
     }
   }
 
-  /**
-   * If CAM-KP-API receives a qualifier constraint that it does not support, it MUST return an empty response (since no
-   * edges meet the constraint).
-   *
-   * (This is still in development at https://github.com/NCATSTranslator/ReasonerAPI/pull/364)
-   */
+  /** If CAM-KP-API receives a qualifier constraint that it does not support, it MUST return an empty response (since no edges meet the
+    * constraint).
+    *
+    * (This is still in development at https://github.com/NCATSTranslator/ReasonerAPI/pull/364)
+    */
   val testUnsupportedQualifierConstraints = suite("testUnsupportedConstraints") {
     // Examples taken from https://github.com/NCATSTranslator/ReasonerAPI/blob/7520ac564e63289dffe092d4c7affd6db4ba22f1/examples/Message/subject_and_object_qualifiers.json
     val query = """{
@@ -131,7 +129,6 @@ object TRAPITest extends DefaultRunnableSpec with LazyLogging {
                      }
                    }"""
 
-
     testM("test unsupported qualifier constraint on QEdge") {
       for {
         biolinkData <- biolinkData
@@ -162,11 +159,11 @@ object TRAPITest extends DefaultRunnableSpec with LazyLogging {
         )
 
         logs = trapiResponse.logs
-        logWarningOfQualifierConstraints = logs.getOrElse(List()).filter({
+        logWarningOfQualifierConstraints = logs.getOrElse(List()).filter {
           // We've made this up ourselves.
           case LogEntry(_, Some("Warning"), Some("UnsupportedQualifierConstraint"), _) => true
-          case _ => false
-        })
+          case _                                                                       => false
+        }
       } yield assert(response.status)(Assertion.hasField("isSuccess", _.isSuccess, Assertion.isTrue)) &&
         assert(content)(Assertion.isNonEmptyString) &&
         // Should return an overall status of Success
@@ -179,76 +176,10 @@ object TRAPITest extends DefaultRunnableSpec with LazyLogging {
     }
   }
 
-  /** This query contains two unexpected properties:
-    *   - unknown_qnode_property on the "n0" QNode
-    *   - unknown_qedge_property on the "e0" QEdge As per https://github.com/NCATSTranslator/ReasonerAPI/pull/322, a server receiving an
-    *     unknown property SHOULD generate a warning and MAY continue processing.
-    */
-  val testUnexpectedPropertiesOnQNodeAndQEdge = suite("testUnexpectedPropertiesOnQNodeAndQEdge") {
-    //
-    val query = """{
-                     "message": {
-                       "query_graph": {
-                         "nodes": {
-                           "n0": {
-                             "categories": ["biolink:GeneOrGeneProduct"],
-                             "unknown_qnode_property": "A1"
-                           },
-                           "n1": { "categories": ["biolink:AnatomicalEntity"], "ids": ["GO:0005634"] }
-                         },
-                         "edges": {
-                           "e0": {
-                             "subject": "n0",
-                             "object": "n1",
-                             "predicates": ["biolink:part_of"],
-                             "unknown_qedge_property": "B2"
-                           }
-                         }
-                       }
-                     }
-                   }"""
-
-    testM("test unexpected properties on QNode and QEdge") {
-      for {
-        biolinkData <- biolinkData
-
-        server <- Server.httpApp
-        response <- server(
-          Request(
-            method = Method.POST,
-            uri = Uri.unsafeFromString("/query")
-          )
-            .withHeaders(Accept(MediaType.application.json), `Content-Type`(MediaType.application.json))
-            .withEntity(query)
-        )
-        content <- EntityDecoder.decodeText(response)
-        trapiResponseJson <- ZIO.fromEither(io.circe.parser.parse(content))
-
-        trapiResponse <- ZIO.fromEither(
-          {
-            implicit val decoderIRI: Decoder[IRI] = Implicits.iriDecoder(biolinkData.prefixes)
-            implicit val keyDecoderIRI: KeyDecoder[IRI] = Implicits.iriKeyDecoder(biolinkData.prefixes)
-            implicit val decoderBiolinkClass: Decoder[BiolinkClass] = Implicits.biolinkClassDecoder(biolinkData.classes)
-            implicit val decoderBiolinkPredicate: Decoder[BiolinkPredicate] =
-              Implicits.biolinkPredicateDecoder(biolinkData.predicates)
-            implicit lazy val decoderTRAPIAttribute: Decoder[TRAPIAttribute] = deriveDecoder[TRAPIAttribute]
-
-            trapiResponseJson.as[TRAPIResponse]
-          }
-        )
-
-        logs = trapiResponse.logs
-      } yield assert(response.status)(Assertion.hasField("isSuccess", _.isSuccess, Assertion.isTrue)) &&
-        assert(content)(Assertion.isNonEmptyString) &&
-        assert(logs)(Assertion.isSome(Assertion.isNonEmpty))
-    }
-  }
-
   val configLayer: Layer[Throwable, ZConfig[AppConfig]] = TypesafeConfig.fromDefaultLoader(AppConfig.config)
   val testLayer = HttpClient.makeHttpClientLayer ++ Biolink.makeUtilitiesLayer ++ configLayer >+> SPARQLQueryExecutor.makeCache.toLayer
 
   def spec = suite("TRAPI tests")(
-    testUnexpectedPropertiesOnQNodeAndQEdge,
     testUnsupportedAttributeConstraints,
     testUnsupportedQualifierConstraints
   ).provideCustomLayer(testLayer.mapError(TestFailure.die))
