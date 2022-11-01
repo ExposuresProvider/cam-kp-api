9a47a1ba751f0b76ea15901e587db9363836f7fc
Gaurav Vaidya <gaurav@renci.org>
2022-11-01 19:01:53 -0400

    Cleaned up code using scalafmtAll.

--- src/main/scala/org/renci/cam/LookupService.scala
+++ src/main/scala/org/renci/cam/LookupService.scala
@@ -9,7 +9,7 @@ import io.circe.{Decoder, Encoder, Json, KeyDecoder, KeyEncoder}
 import org.apache.jena.query.QuerySolution
 import org.apache.jena.rdf.model.{RDFNode, Resource}
 import org.http4s.{HttpRoutes, InvalidBodyException}
-import org.renci.cam.Biolink.{BiolinkData, biolinkData}
+import org.renci.cam.Biolink.{biolinkData, BiolinkData}
 import org.renci.cam.SPARQLQueryExecutor.SPARQLCache
 import org.renci.cam.Server.EndpointEnv
 import org.renci.cam.Server.LocalTapirJsonCirce.jsonBody
@@ -35,12 +35,16 @@ object LookupService extends LazyLogging {
   /* DATA STRUCTURES */
 
   /** This case class represents a single triple from our database.
-   *
-   * @param s Subject
-   * @param p Predicate
-   * @param o Object
-   * @param g Graph
-   */
+    *
+    * @param s
+    *   Subject
+    * @param p
+    *   Predicate
+    * @param o
+    *   Object
+    * @param g
+    *   Graph
+    */
   case class ResultTriple(
     s: String,
     p: String,
@@ -55,22 +59,25 @@ object LookupService extends LazyLogging {
     result.get("g").toString
   )
 
-  /**
-   * This case class represents a relation between the subject object and other objects in CAM-KP.
-   *
-   * This should really be a TRAPI object, but since there isn't a TRAPI object that includes both a node
-   * and an edge, we make one up here.
-   *
-   * @param subj The subject of this relation.
-   * @param preds The predicates connecting this subject to this object.
-   * @param biolinkPredicates The predicates in `pred` translated into Biolink predicates. The predicate IRIs are used
-   *                          as the keys.
-   * @param obj The object of this relation.
-   * @param objRelations The relations connected to the object of this relation. CAM-KP is designed so that this
-   *    list is not necessary, since `subj` should be connected to every other entity in its graph (even if only by a
-   *    `biolink:related_to` predicate); however, it is intended to test whether this is in fact the case.
-   * @param g The graph that this relation comes from.
-   */
+  /** This case class represents a relation between the subject object and other objects in CAM-KP.
+    *
+    * This should really be a TRAPI object, but since there isn't a TRAPI object that includes both a node and an edge, we make one up here.
+    *
+    * @param subj
+    *   The subject of this relation.
+    * @param preds
+    *   The predicates connecting this subject to this object.
+    * @param biolinkPredicates
+    *   The predicates in `pred` translated into Biolink predicates. The predicate IRIs are used as the keys.
+    * @param obj
+    *   The object of this relation.
+    * @param objRelations
+    *   The relations connected to the object of this relation. CAM-KP is designed so that this list is not necessary, since `subj` should
+    *   be connected to every other entity in its graph (even if only by a `biolink:related_to` predicate); however, it is intended to test
+    *   whether this is in fact the case.
+    * @param g
+    *   The graph that this relation comes from.
+    */
   case class Relation(
     subj: LabeledIRI,
     preds: Set[LabeledIRI],
@@ -80,26 +87,28 @@ object LookupService extends LazyLogging {
     g: Set[String]
   )
 
-  /**
-   * The result of calling `/lookup` on an identifier.
-   *
-   * @param queryId The identifier queried with lookup.
-   * @param normalizedId The identifier actually used for the query. This will be identical to queryId if normalization
-   *                     was not requested or
-   * @param biolinkPredicates All predicates connected to the identifier being looked up.
-   * @param relations All relations connected to the identifier being looked up.
-   * @param subjectTriples All subject triples for the identifier being looked up (i.e. triples in the triplestore
-   *                       in the shape `normalizedId ?p ?o`).
-   * @param objectTriples All object triples for the identifier being looked up (i.e. triples in the triplestore
-   *                      in the shape `?s ?p normalizedId`)
-   */
+  /** The result of calling `/lookup` on an identifier.
+    *
+    * @param queryId
+    *   The identifier queried with lookup.
+    * @param normalizedId
+    *   The identifier actually used for the query. This will be identical to queryId if normalization was not requested or
+    * @param biolinkPredicates
+    *   All predicates connected to the identifier being looked up.
+    * @param relations
+    *   All relations connected to the identifier being looked up.
+    * @param subjectTriples
+    *   All subject triples for the identifier being looked up (i.e. triples in the triplestore in the shape `normalizedId ?p ?o`).
+    * @param objectTriples
+    *   All object triples for the identifier being looked up (i.e. triples in the triplestore in the shape `?s ?p normalizedId`)
+    */
   case class Result(
-                     queryId: String,
-                     normalizedId: LabeledIRI,
-                     biolinkPredicates: Set[BiolinkPredicate],
-                     relations: Seq[Relation],
-                     subjectTriples: Seq[ResultTriple],
-                     objectTriples: Seq[ResultTriple]
+    queryId: String,
+    normalizedId: LabeledIRI,
+    biolinkPredicates: Set[BiolinkPredicate],
+    relations: Seq[Relation],
+    subjectTriples: Seq[ResultTriple],
+    objectTriples: Seq[ResultTriple]
   )
 
   /** An error returned by calling `/lookup`. */
@@ -109,10 +118,12 @@ object LookupService extends LazyLogging {
   )
 
   /** A labeled IRI is an IRI with an optional label.
-   *
-   * @param iri An IRI.
-   * @param label An optional label for this IRI.
-   */
+    *
+    * @param iri
+    *   An IRI.
+    * @param label
+    *   An optional label for this IRI.
+    */
   case class LabeledIRI(
     iri: String,
     label: Set[String]
@@ -124,28 +135,34 @@ object LookupService extends LazyLogging {
 
   /* Helper methods */
 
-  /**
-   * Returns a list of Relations for a particular subject.
-   *
-   * This function is intended to be called recursively: it will call itself to get all the relations for every subject
-   * of a relation.
-   *
-   * @param subject The subject IRI to find relations for.
-   * @param hopLimit The number of hops to find relations for. If equal to zero, this will return an empty Seq.
-   * @param subjectsPreviouslyQueried Subject IRIs that have already been queried. We will not recurse into these subject IRIs.
-   * @return A ZIO that returns a Relation, possibly containing multiple Relations in its objRelations field.
-   */
-  def getRelations(subject: String, hopLimit: Int, subjectsPreviouslyQueried: Set[String]): ZIO[ZConfig[AppConfig] with HttpClient with Has[BiolinkData], Throwable, Iterable[Relation]] = for {
-    biolinkData <- biolinkData
+  /** Returns a list of Relations for a particular subject.
+    *
+    * This function is intended to be called recursively: it will call itself to get all the relations for every subject of a relation.
+    *
+    * @param subject
+    *   The subject IRI to find relations for.
+    * @param hopLimit
+    *   The number of hops to find relations for. If equal to zero, this will return an empty Seq.
+    * @param subjectsPreviouslyQueried
+    *   Subject IRIs that have already been queried. We will not recurse into these subject IRIs.
+    * @return
+    *   A ZIO that returns a Relation, possibly containing multiple Relations in its objRelations field.
+    */
+  def getRelations(
+    subject: String,
+    hopLimit: Int,
+    subjectsPreviouslyQueried: Set[String]): ZIO[ZConfig[AppConfig] with HttpClient with Has[BiolinkData], Throwable, Iterable[Relation]] =
+    for {
+      biolinkData <- biolinkData
 
-    subjectIRI = {
-      implicit val iriKeyDecoder: KeyDecoder[IRI] = Implicits.iriKeyDecoder(biolinkData.prefixes)
+      subjectIRI = {
+        implicit val iriKeyDecoder: KeyDecoder[IRI] = Implicits.iriKeyDecoder(biolinkData.prefixes)
 
-      iriKeyDecoder(subject).get
-    }
+        iriKeyDecoder(subject).get
+      }
 
-    relationsQuery =
-      sparql"""PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+      relationsQuery =
+        sparql"""PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
                PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
 
                SELECT DISTINCT ?subjLabel ?p ?pLabel ?obj ?objLabel ?g {
@@ -160,66 +177,72 @@ object LookupService extends LazyLogging {
                   OPTIONAL { ?p rdfs:label ?pLabel }
                   OPTIONAL { ?obj rdfs:label ?objLabel }
                }"""
-    relationsResults <- SPARQLQueryExecutor.runSelectQuery(relationsQuery.toQuery)
-    preds = relationsResults.map(_.getResource("p").getURI).map(IRI(_))
-    biolinkRelationMap <- QueryService.mapRelationsToLabelAndBiolink(preds.toSet)
+      relationsResults <- SPARQLQueryExecutor.runSelectQuery(relationsQuery.toQuery)
+      preds = relationsResults.map(_.getResource("p").getURI).map(IRI(_))
+      biolinkRelationMap <- QueryService.mapRelationsToLabelAndBiolink(preds.toSet)
 
-    // We want to group these by object, so we don't return a gazillion predicates for each result.
-    objectMap = relationsResults.groupBy(_.getResource("obj").getURI)
+      // We want to group these by object, so we don't return a gazillion predicates for each result.
+      objectMap = relationsResults.groupBy(_.getResource("obj").getURI)
 
-    relations = objectMap map { case (obj, results) =>
-      val predResults = results map { res =>
-        val predIRI = res.getResource("p").getURI
+      relations = objectMap map { case (obj, results) =>
+        val predResults = results map { res =>
+          val predIRI = res.getResource("p").getURI
 
-        val pred = res.getLiteral("pLabel") match {
-          case null => LabeledIRI(predIRI, Set())
-          case lit => LabeledIRI(predIRI, Set(lit.getString))
+          val pred = res.getLiteral("pLabel") match {
+            case null => LabeledIRI(predIRI, Set())
+            case lit  => LabeledIRI(predIRI, Set(lit.getString))
+          }
+
+          val biolinkRes = biolinkRelationMap.get(IRI(predIRI))
+          val biolink = biolinkRes match {
+            case None                     => Set[LabeledIRI]()
+            case Some((None, iri))        => Set(LabeledIRI(iri.value, Set()))
+            case Some((Some(label), iri)) => Set(LabeledIRI(iri.value, Set(label)))
+          }
+
+          (pred, biolink)
         }
 
-        val biolinkRes = biolinkRelationMap.get(IRI(predIRI))
-        val biolink = biolinkRes match {
-          case None => Set[LabeledIRI]()
-          case Some((None, iri)) => Set(LabeledIRI(iri.value, Set()))
-          case Some((Some(label), iri)) => Set(LabeledIRI(iri.value, Set(label)))
-        }
+        val objLabeled = results
+          .map({ res =>
+            res.getLiteral("objLabel") match {
+              case null => LabeledIRI(obj, Set())
+              case lit  => LabeledIRI(obj, Set(lit.getString))
+            }
+          })
+          .toSet
 
-        (pred, biolink)
+        val subjLabeled = results
+          .flatMap({ res =>
+            res.getLiteral("subjLabel") match {
+              case null => Set()
+              case lit  => Set(lit.getString)
+            }
+          })
+          .toSet
+
+        Relation(
+          LabeledIRI(subjectIRI.value, subjLabeled),
+          predResults.map(_._1).toSet,
+          predResults.map(kv => (kv._1.iri, kv._2)).toMap,
+          objLabeled,
+          Seq[Relation](), // TODO: recurse!
+          results.map(_.getResource("g").getURI).toSet
+        )
       }
+    } yield relations
 
-      val objLabeled = results.map({ res =>
-        res.getLiteral("objLabel") match {
-          case null => LabeledIRI(obj, Set())
-          case lit => LabeledIRI(obj, Set(lit.getString))
-        }
-      }).toSet
-
-      val subjLabeled = results.flatMap({ res =>
-        res.getLiteral("subjLabel") match {
-          case null => Set()
-          case lit => Set(lit.getString)
-        }
-      }).toSet
-
-      Relation(
-        LabeledIRI(subjectIRI.value, subjLabeled),
-        predResults.map(_._1).toSet,
-        predResults.map(kv => (kv._1.iri, kv._2)).toMap,
-        objLabeled,
-        Seq[Relation](), // TODO: recurse!
-        results.map(_.getResource("g").getURI).toSet
-      )
-    }
-  } yield relations
-
-  /**
-   * Lookup a particular subject.
-   *
-   * @param subject The IRI of a subject to investigate.
-   * @param nodeNormURL The URL of NodeNorm to call for normalization (should end with '/get_normalized_nodes'),
-   *                    or None if no normalization is required.
-   * @param hopLimit The maximum number of hops to follow.
-   * @return A ZIO returning a Result to the user.
-   */
+  /** Lookup a particular subject.
+    *
+    * @param subject
+    *   The IRI of a subject to investigate.
+    * @param nodeNormURL
+    *   The URL of NodeNorm to call for normalization (should end with '/get_normalized_nodes'), or None if no normalization is required.
+    * @param hopLimit
+    *   The maximum number of hops to follow.
+    * @return
+    *   A ZIO returning a Result to the user.
+    */
   def lookup(subject: String, nodeNormURL: Option[String], hopLimit: Int): ZIO[EndpointEnv, Throwable, Result] = for {
     biolinkData <- biolinkData
 
@@ -290,7 +313,6 @@ object LookupService extends LazyLogging {
             .default(10)
             .example(10)
         )
-
         .out(jsonBody[Result])
         .errorOut(jsonBody[Error])
         .summary("Look up relationships within CAM-KP-API by identifier.")
@@ -298,7 +320,10 @@ object LookupService extends LazyLogging {
 
   def lookupRouteR(lookupEndpoint: LookupEndpoint): HttpRoutes[RIO[EndpointEnv, *]] =
     ZHttp4sServerInterpreter[EndpointEnv]()
-      .from(lookupEndpoint) { case (subj, nodeNormURL, hopLimit) => lookup(subj, nodeNormURL, hopLimit)
-      .mapError(ex => Error("interp_error", ex.getMessage)) }
+      .from(lookupEndpoint) { case (subj, nodeNormURL, hopLimit) =>
+        lookup(subj, nodeNormURL, hopLimit)
+          .mapError(ex => Error("interp_error", ex.getMessage))
+      }
       .toRoutes
+
 }
