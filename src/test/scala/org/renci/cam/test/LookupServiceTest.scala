cbd2c2f1f63acdf9ff5c82294434de58d1908e1e
Gaurav Vaidya <gaurav@ggvaidya.com>
2022-11-02 02:36:03 -0400

    Added support for running tests on a list of lookup-service-ids.

--- src/test/scala/org/renci/cam/test/LookupServiceTest.scala
+++ src/test/scala/org/renci/cam/test/LookupServiceTest.scala
@@ -10,6 +10,7 @@ import org.http4s.Method.GET
 import org.http4s.client.dsl.io._
 import org.http4s.{EntityDecoder, Request, Uri}
 import org.renci.cam.HttpClient.HttpClient
+import org.renci.cam.Server.EndpointEnv
 import org.renci.cam._
 import org.renci.cam.domain._
 import zio.blocking.effectBlockingIO
@@ -17,10 +18,12 @@ import zio.cache.Cache
 import zio.config.ZConfig
 import zio.config.typesafe.TypesafeConfig
 import zio.interop.catz.concurrentInstance
+import zio.stream.ZStream
 import zio.test.{assert, _}
-import zio.{Layer, ZIO, ZLayer}
+import zio.{Layer, Task, ZIO, ZLayer}
 
 import java.io.{File, FileWriter, PrintWriter}
+import scala.io.Source
 
 /** Test the `/lookup` endpoint (implemented by LookupService)
   */
@@ -47,30 +50,41 @@ object LookupServiceTest extends DefaultRunnableSpec with LazyLogging {
       }
     }
 
-  def testIdentifier(id: String) = suite(s"Test identifier ${id}") {
-    testM("Test whether we can retrieve results for this identifier") {
+  def testIdentifier(id: String): ZSpec[EndpointEnv, Throwable] =
+    testM(s"Test whether we can retrieve results for identifier ${id}") {
       val idFilenameSafe = id.replaceAll("\\W", "_")
       val outputFile = new File(s"./src/test/resources/lookup-service-results/${idFilenameSafe}.json")
 
       for {
-
         server <- Server.httpApp
         response <- server(Request(GET, Uri.unsafeFromString("/lookup").withQueryParam("subject", id)))
         content <- EntityDecoder.decodeText(response)
         resultOrErrorJson <- ZIO.fromEither(io.circe.parser.parse(content))
         result <- ZIO.fromEither(resultOrErrorJson.as[LookupService.Result])
         _ = logger.debug(s"Results for identifier ${id}: ${result.asJson.deepDropNullValues.spaces2SortKeys}")
-        _ <- effectBlockingIO(new PrintWriter(new FileWriter(outputFile)))
+        fileWritten <- effectBlockingIO(new PrintWriter(new FileWriter(outputFile)))
           .bracketAuto { pw =>
             pw.println(result.asJson.deepDropNullValues.spaces2SortKeys)
-            ZIO.unit
+            ZIO.succeed(true)
           }
       } yield assert(content)(Assertion.isNonEmptyString) &&
         assert(result.subjectTriples)(Assertion.isNonEmpty) &&
         assert(result.objectTriples)(Assertion.isNonEmpty) &&
-        assert(result.relations)(Assertion.isNonEmpty)
+        assert(result.relations)(Assertion.isNonEmpty) &&
+        assert(fileWritten)(Assertion.isTrue)
     }
-  }
+
+  val lookupServiceIdFile = new File("./src/test/resources/lookup-service-ids.txt")
+
+  val testIdentifiersInFile: Spec[EndpointEnv, TestFailure[Throwable], TestSuccess] =
+    suiteM(s"Test identifiers in ${lookupServiceIdFile}") {
+      // noinspection SourceNotClosed
+      ZStream
+        .fromIteratorEffect(Task(Source.fromFile(lookupServiceIdFile).getLines()))
+        .map(testIdentifier)
+        .runCollect
+        .mapError(TestFailure.fail)
+    }
 
   val configLayer: Layer[Throwable, ZConfig[AppConfig]] = TypesafeConfig.fromDefaultLoader(AppConfig.config)
 
@@ -82,7 +96,7 @@ object LookupServiceTest extends DefaultRunnableSpec with LazyLogging {
 
   def spec: Spec[environment.TestEnvironment, TestFailure[Throwable], TestSuccess] = suite("Limit tests")(
     testDefault,
-    testIdentifier("CHEBI:17685")
+    testIdentifiersInFile
   ).provideCustomLayer(testLayer.mapError(TestFailure.die))
 
 }
