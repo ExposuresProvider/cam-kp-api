aa94698c074a1cc7f69599cba65a83af3c58e426
Gaurav Vaidya <gaurav@ggvaidya.com>
2022-11-02 02:11:03 -0400

    Added a per-identifier test.

--- src/test/scala/org/renci/cam/test/LookupServiceTest.scala
+++ src/test/scala/org/renci/cam/test/LookupServiceTest.scala
@@ -6,16 +6,21 @@ import io.circe.generic.auto._
 import io.circe.generic.semiauto._
 import io.circe.syntax.EncoderOps
 import org.apache.jena.query.{Query, QuerySolution}
+import org.http4s.Method.GET
+import org.http4s.client.dsl.io._
 import org.http4s.{EntityDecoder, Request, Uri}
 import org.renci.cam.HttpClient.HttpClient
 import org.renci.cam._
 import org.renci.cam.domain._
+import zio.blocking.effectBlockingIO
 import zio.cache.Cache
 import zio.config.ZConfig
 import zio.config.typesafe.TypesafeConfig
 import zio.interop.catz.concurrentInstance
 import zio.test.{assert, _}
 import zio.{Layer, ZIO, ZLayer}
+
+import java.io.{File, FileWriter, PrintWriter}
 
 /** Test the `/lookup` endpoint (implemented by LookupService)
   */
@@ -33,7 +38,7 @@ object LookupServiceTest extends DefaultRunnableSpec with LazyLogging {
           resultOrErrorJson <- ZIO.fromEither(io.circe.parser.parse(content))
 
           result <- ZIO.fromEither(resultOrErrorJson.as[LookupService.Result])
-          _ = logger.info(s"Results: ${result.asJson.deepDropNullValues.spaces2SortKeys}")
+          _ = logger.debug(s"Results: ${result.asJson.deepDropNullValues.spaces2SortKeys}")
         } yield assert(content)(Assertion.isNonEmptyString) &&
           assert(result.subjectTriples)(Assertion.isNonEmpty) &&
           assert(result.objectTriples)(Assertion.isNonEmpty) &&
@@ -41,6 +46,31 @@ object LookupServiceTest extends DefaultRunnableSpec with LazyLogging {
           assert(result.biolinkPredicates)(Assertion.isEmpty)
       }
     }
+
+  def testIdentifier(id: String) = suite(s"Test identifier ${id}") {
+    testM("Test whether we can retrieve results for this identifier") {
+      val idFilenameSafe = id.replaceAll("\\W", "_")
+      val outputFile = new File(s"./src/test/resources/lookup-service-results/${idFilenameSafe}.json")
+
+      for {
+
+        server <- Server.httpApp
+        response <- server(Request(GET, Uri.unsafeFromString("/lookup").withQueryParam("subject", id)))
+        content <- EntityDecoder.decodeText(response)
+        resultOrErrorJson <- ZIO.fromEither(io.circe.parser.parse(content))
+        result <- ZIO.fromEither(resultOrErrorJson.as[LookupService.Result])
+        _ = logger.debug(s"Results for identifier ${id}: ${result.asJson.deepDropNullValues.spaces2SortKeys}")
+        _ <- effectBlockingIO(new PrintWriter(new FileWriter(outputFile)))
+          .bracketAuto { pw =>
+            pw.println(result.asJson.deepDropNullValues.spaces2SortKeys)
+            ZIO.unit
+          }
+      } yield assert(content)(Assertion.isNonEmptyString) &&
+        assert(result.subjectTriples)(Assertion.isNonEmpty) &&
+        assert(result.objectTriples)(Assertion.isNonEmpty) &&
+        assert(result.relations)(Assertion.isNonEmpty)
+    }
+  }
 
   val configLayer: Layer[Throwable, ZConfig[AppConfig]] = TypesafeConfig.fromDefaultLoader(AppConfig.config)
 
@@ -51,7 +81,8 @@ object LookupServiceTest extends DefaultRunnableSpec with LazyLogging {
     HttpClient.makeHttpClientLayer ++ Biolink.makeUtilitiesLayer ++ configLayer >+> SPARQLQueryExecutor.makeCache.toLayer
 
   def spec: Spec[environment.TestEnvironment, TestFailure[Throwable], TestSuccess] = suite("Limit tests")(
-    testDefault
+    testDefault,
+    testIdentifier("CHEBI:17685")
   ).provideCustomLayer(testLayer.mapError(TestFailure.die))
 
 }
