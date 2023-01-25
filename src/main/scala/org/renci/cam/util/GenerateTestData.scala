8a78b1c39ad3d9990c855a733fc5c17d685af5b2
Gaurav Vaidya <gaurav@renci.org>
2023-01-25 04:47:06 -0500

    First stab at a TestEdge generator.

--- src/main/scala/org/renci/cam/util/GenerateTestData.scala
+++ src/main/scala/org/renci/cam/util/GenerateTestData.scala
@@ -6,17 +6,20 @@ import io.circe.generic.auto._
 import io.circe.syntax._
 import org.apache.commons.csv.CSVFormat
 import org.http4s.implicits._
+import org.phenoscape.sparql.SPARQLInterpolation._
+import org.renci.cam.Biolink.biolinkData
+import org.renci.cam.QueryService.RDFSSubClassOf
 import org.renci.cam._
-import org.renci.cam.domain.{BiolinkClass, BiolinkPredicate, TRAPIQueryEdge, TRAPIQueryGraph, TRAPIQueryNode}
+import org.renci.cam.domain.{BiolinkClass, BiolinkPredicate, IRI, TRAPIKnowledgeGraph, TRAPIMessage, TRAPIQueryEdge, TRAPIQueryGraph, TRAPIQueryNode, TRAPIResponse}
 import zio._
-import zio.blocking.{Blocking, effectBlockingIO}
+import zio.blocking.{effectBlockingIO, Blocking}
+import zio.config.ZConfig
 import zio.config.typesafe.TypesafeConfig
-import zio.config.{ZConfig, getConfig}
 import zio.interop.catz._
 import zio.interop.catz.implicits._
-import zio.stream.ZStream
+import zio.stream.{ZSink, ZStream}
 
-import java.io.StringReader
+import java.io.{File, StringReader}
 import java.nio.charset.StandardCharsets
 import scala.io.Source
 import scala.jdk.javaapi.CollectionConverters
@@ -26,6 +29,8 @@ import scala.jdk.javaapi.CollectionConverters
   * generate this data.
   */
 object GenerateTestData extends zio.App with LazyLogging {
+
+  val LinkMLClassDefinition = IRI("https://w3id.org/linkml/ClassDefinition")
 
   override lazy val logger = Logger(GenerateTestData.getClass.getSimpleName);
 
@@ -101,40 +106,132 @@ object GenerateTestData extends zio.App with LazyLogging {
 
    */
 
+  case class ParentChildRelation(
+    parent: String,
+    child: String
+  )
+
   override def run(args: List[String]): URIO[zio.ZEnv, ExitCode] = {
+    if (args.length > 1) {
+      throw new RuntimeException("Only a single argument is allowed: the test-edges JSONL file to write.")
+    }
+
+    val testEdgesFile = if (args.length == 1) args(1) else "src/main/resources/test-edges.jsonl"
+
+    case class TestEdge(
+      subject_category: String,
+      object_category: String,
+      predicate: String,
+      subject: String,
+      `object`: String
+    )
+
     val program = for {
-      conf <- getConfig[AppConfig]
+      biolinkData <- biolinkData
+
+      // Get a list of all the Biolink parent-child relationships we know about.
+      parent_child_query =
+        sparql"""SELECT ?parent ?child WHERE {
+            ?child ${RDFSSubClassOf} ?parent .
+            FILTER(?child != ?parent)
+
+            ?child a ${LinkMLClassDefinition} .
+            ?parent a ${LinkMLClassDefinition} .
+          }
+          """
+      parent_child_relations_solutions <- SPARQLQueryExecutor.runSelectQuery(parent_child_query.toQuery)
+      parent_child_relations = parent_child_relations_solutions
+        .map { qs =>
+          (
+            qs.getResource("parent").getURI.replaceFirst("https://w3id.org/biolink/vocab/", "biolink:"),
+            qs.getResource("child").getURI.replaceFirst("https://w3id.org/biolink/vocab/", "biolink:")
+          )
+        }
+        .groupMap(_._1)(_._2)
+      _ = logger.info(f"Parent-child relations: ${parent_child_relations}")
 
       // Get a list of all the predicates in predicates.csv.
       predicates <- effectBlockingIO(Source.fromInputStream(getClass.getResourceAsStream("/predicates.csv"), StandardCharsets.UTF_8.name()))
         .bracketAuto { source =>
           effectBlockingIO(source.getLines().mkString("\n"))
         }
-      predicateRecords <- Task.effect(CSVFormat.DEFAULT.parse(new StringReader(predicates)).getRecords)
+      predicateRecords <- Task.effect(CollectionConverters.asScala(CSVFormat.DEFAULT.parse(new StringReader(predicates)).getRecords))
 
       // Generate a test record for each record.
       results <- ZStream
-        .fromIterable(CollectionConverters.asScala(predicateRecords))
-        .flatMap(predicateRecord =>
-          ZStream.fromEffect(
-            QueryService.run(
-              100,
-              TRAPIQueryGraph(
-                Map(
-                  "n0" -> TRAPIQueryNode(None, Some(List(BiolinkClass(predicateRecord.get(0)))), None),
-                  "n1" -> TRAPIQueryNode(None, Some(List(BiolinkClass(predicateRecord.get(2)))), None)
-                ),
-                Map(
-                  "e0" -> TRAPIQueryEdge(Some(List(BiolinkPredicate("related_to"))), "n0", "n1", None)
+        .fromIterable(predicateRecords)
+        .filter { predicateRecord =>
+          val subj = s"biolink:${predicateRecord.get(0)}"
+          val obj = s"biolink:${predicateRecord.get(2)}"
+
+          if (parent_child_relations.contains(subj)) {
+            logger.info(f"Skipping ${predicateRecord}, as subject has children: ${parent_child_relations.getOrElse(subj, List())}")
+            false
+          } else if (parent_child_relations.contains(obj)) {
+            logger.info(f"Skipping ${predicateRecord}, as object has children: ${parent_child_relations.getOrElse(obj, List())}")
+            false
+          } else true
+        }
+        .flatMap { predicateRecord =>
+          val subj = s"biolink:${predicateRecord.get(0)}"
+          val obj = s"biolink:${predicateRecord.get(2)}"
+
+          ZStream
+            .fromEffect(
+              QueryService.run(
+                1,
+                TRAPIQueryGraph(
+                  Map(
+                    "n0" -> TRAPIQueryNode(None, Some(List(BiolinkClass(subj))), None),
+                    "n1" -> TRAPIQueryNode(None, Some(List(BiolinkClass(obj))), None)
+                  ),
+                  Map(
+                    "e0" -> TRAPIQueryEdge(Some(List(BiolinkPredicate("related_to"))), "n0", "n1", None)
+                  )
                 )
               )
             )
-          ))
-        .tap { resp =>
-          logger.info(f"Found TRAPI Response: " + resp)
-          ZIO.succeed(resp)
+            .mapError { err =>
+              val errMsg = f"Caught error while querying ${predicateRecord}: ${err}"
+              logger.error(errMsg)
+              TRAPIResponse(TRAPIMessage(None, None, None), Some("Error"), Some(errMsg), None)
+            }
+            .tap { resp =>
+              logger.info(f"TRAPI Response: ${resp}")
+
+              val results = resp.message.results.getOrElse(List())
+              logger.info(f"TRAPI results contains ${results.size}: ${results}")
+
+              ZIO.succeed(resp)
+            }
+            .flatMap(resp =>
+              ZStream.fromIterable(
+                resp.message.knowledge_graph.getOrElse(TRAPIKnowledgeGraph(Map(), Map())).edges.values.map { edge =>
+                  TestEdge(
+                    subj,
+                    obj,
+                    Implicits.compactIRIIfPossible(edge.predicate.getOrElse(BiolinkPredicate("example:error")).iri, biolinkData.prefixes),
+                    Implicits.compactIRIIfPossible(edge.subject, biolinkData.prefixes),
+                    Implicits.compactIRIIfPossible(edge.`object`, biolinkData.prefixes)
+                  )
+                }
+              ))
+            .tap { edge =>
+              logger.info(f"TestEdge found: ${edge}")
+              ZIO.succeed(edge)
+            }
         }
-        .runCollect
+        .map { edge =>
+          val edgeAsStr = edge.asJson.noSpacesSortKeys
+
+          logger.info(f"Converted test edge to JSON: ${edgeAsStr}")
+
+          edgeAsStr
+        }
+        .intersperse("\n")
+        .run(ZSink
+          .fromFile(new File(testEdgesFile).toPath)
+          .contramapChunks[String](_.flatMap(_.getBytes)))
     } yield ({
       logger.info(f"Found result: " + results)
     })
